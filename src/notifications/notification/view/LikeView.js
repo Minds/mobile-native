b25d58c98851b48fb9161d36e4724aecd47a1c24
Martin Santangelo <msantang78@gmail.com>
2017-11-24 10:26:44 -0300

    (fix) Like Notification component

--- src/notifications/notification/view/LikeView.js
+++ src/notifications/notification/view/LikeView.js
@@ -41,30 +41,31 @@ export default class LikeView extends Component {
     switch (entity.entityObj.type) {
       case "comment":
         return (
-          <View>
             <Text>{entity.fromObj.name} down voted <Text style={styles.link}> your comment </Text></Text>
-          </View>
         )
       case "activity":
-        return (
-          <View>
-            entity.entityObj.title && <Text> {entity.fromObj.name} down voted <Text style={styles.link}>{ entity.entityObj.title }</Text></Text>
-            !entity.entityObj.title && <Text> {entity.fromObj.name} down voted <Text style={styles.link}>your activity</Text></Text>
-          </View>
-        )
+        if (entity.entityObj.title) {
+          return (
+            <Text> {entity.fromObj.name} down voted <Text style={styles.link}>{entity.entityObj.title}</Text></Text>
+          )
+        } else {
+          return (
+            <Text> {entity.fromObj.name} down voted <Text style={styles.link}>your activity</Text></Text>
+          )
+        }
       case "object":
-        return (
-          <View>
-            <Text>{entity.fromObj.name}</Text>
-            entity.entityObj.title && <Text style={styles.link}>{entity.entityObj.title}</Text>
-            !entity.entityObj.title && <Text style={styles.link}> your {entity.entityObj.subtype}</Text>
-          </View>
-        )
+        if (entity.entityObj.title) {
+          return (
+            <Text> {entity.fromObj.name} <Text style={styles.link}>{entity.entityObj.title}</Text></Text>
+          )
+        } else {
+          return (
+            <Text> {entity.fromObj.name} <Text style={styles.link}>your {entity.entityObj.subtype}</Text></Text>
+          )
+        }
       default:
         return (
-          <View>
             <Text>... oops.</Text>
-          </View>
         )
     }
   }
