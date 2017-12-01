0e14f6f8b861a9865b672c3de92983ef82181de4
Martin Santangelo <msantang78@gmail.com>
2017-12-01 14:31:33 -0300

    (refactor) Split channel screen in smaller components

--- src/channel/ChannelScreen.js
+++ src/channel/ChannelScreen.js
@@ -22,103 +22,52 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import { MINDS_URI } from '../config/Config';
 
 import abbrev from '../common/helpers/abbrev';
-import Carousel from '../common/components/Carousel';
+import RewardsCarousel from './carousel/RewardsCarousel';
 
+import ChannelHeader from './header/ChannelHeader';
+
+/**
+ * Channel Screen
+ */
 @inject('channel')
 @observer
 export default class ChannelScreen extends Component {
 
-  // no NavBar
+  /**
+   * Disable navigation bar
+   */
   static navigationOptions = {
     header: null
   }
-  // loading
+
+  /**
+   * Load data on mount
+   */
   componentWillMount() {
     const guid = this.props.navigation.state.params.guid;
     this.props.channel.load(guid);
     this.props.channel.loadrewards(guid);
   }
 
-  getBannerFromChannel() {
-    const channel = this.props.channel.channel;
-    if (channel && channel.carousels) {
-      return channel.carousels[0].src;
-    }
-
-    return `${MINDS_URI}fs/v1/banners/${channel.guid}/0/${channel.banner}`;
-  }
-
-  getAvatar() {
-    const channel = this.props.channel.channel;
-    return `${MINDS_URI}icon/${channel.guid}/large/${channel.icontime}`;
-  }
-
+  /**
+   * Render
+   */
   render() {
     const channel = this.props.channel.channel;
     const rewards = this.props.channel.rewards;
 
     if (!channel.guid) {
       return (
-      <ActivityIndicator size={'large'} />
+        <ActivityIndicator size={'large'} />
       );
-    }
-
-    const name = this.props.navigation.state.params.guid;
-    const avatar = {uri:this.getAvatar()};
-    const iurl = {uri:this.getBannerFromChannel()};
-
-    const rewardsArray = [];
-
-    if (rewards.money) {
-      rewards.money.map((rew) => {
-        rewardsArray.push(
-          <View key={`rewards${rew.amount}`} style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-            <View>
-              <Text style={styles.rewardamount}>${rew.amount}+</Text>
-              <Text style={styles.rewarddesc}>{rew.description}</Text>
-            </View>
-            <Icon style={styles.rewardicon} name={'ios-flash'} size={36}/>
-          </View>
-        );
-      });
     }
 
     return (
       <ScrollView style={styles.container}>
-        <Image source={iurl} style={styles.banner} />
-        <View style={styles.headertextcontainer}>
-          <View style={styles.countercontainer}>
-            <View style={styles.counter}>
-              <Text style={styles.countertitle}>SUBSCRIBERS</Text>
-              <Text>{abbrev(channel.subscribers_count, 0)}</Text>
-            </View>
-            <View style={styles.counter}>
-              <Text style={styles.countertitle}>VIEWS</Text>
-              <Text>{abbrev(channel.impressions, 0)}</Text>
-            </View>
-          </View>
-          <View style={{ flexDirection: 'row'}}>
-            <View style={{flexDirection: 'column', flex:1}}>
-              <Text style={styles.name}>{channel.name.toUpperCase()}</Text>
-              <Text style={styles.username}>@{channel.username}</Text>
-            </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: 100}}>
-              <Button
-                style={{width:80, padding: 5}}
-                onPress={() => {console.log('press')}}
-                title="Message"
-                color="#4791d6"
-                accessibilityLabel="Learn more about this purple button"
-              />
-              <Icon name="md-settings" size={15}/>
-            </View>
-          </View>
-          <Text style={styles.briefdescription}>{channel.briefdescription}</Text>
-          <Carousel style={styles.carousel} height={70} color={'#0071ff'}>
-            {rewardsArray}
-          </Carousel>
+        <ChannelHeader styles={styles} channel={channel} />
+        <RewardsCarousel rewards={rewards} styles={styles}/>
+        <View style={styles.body}>
         </View>
-        <Image source={avatar} style={styles.avatar} />
       </ScrollView>
     );
   }
@@ -134,11 +83,20 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     width: '100%',
   },
+  body: {
+    flexDirection: 'column',
+    flex: 1,
+    alignItems: 'stretch',
+    backgroundColor: '#aaa',
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
   rewardicon: {
     color: '#0071ff',
     width: 30
   },
   rewardamount: {
+    fontSize: 16,
     fontWeight: 'bold'
   },
   rewarddesc: {
@@ -147,14 +105,19 @@ const styles = StyleSheet.create({
   carousel: {
     flex: 1,
   },
+  carouselitems: {
+    paddingLeft: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
   banner: {
     flexDirection: 'row',
     alignItems: 'stretch',
     height: 190,
   },
   briefdescription: {
-    fontSize: 10,
-    paddingTop: 20,
+    fontSize: 12,
+    paddingTop: 25,
     paddingBottom: 20,
     color: '#919191'
   },
@@ -164,16 +127,20 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   username: {
-    fontSize: 8,
+    fontSize: 10,
     color: '#999'
   },
   name: {
     fontWeight: 'bold',
-    fontSize: 15,
+    fontSize: 16,
   },
   countertitle: {
     color: '#666',
     fontSize: 10
+  },
+  countervalue: {
+    paddingTop: 5,
+    fontWeight: 'bold',
   },
   countercontainer: {
     paddingLeft: 130,
---
+++ src/channel/carousel/RewardsCarousel.js
@@ -0,0 +1,56 @@
+import React, {
+  Component
+} from 'react';
+
+import {
+  Text,
+  View,
+  Dimensions
+} from 'react-native';
+
+import {
+  observer,
+} from 'mobx-react/native'
+
+import Icon from 'react-native-vector-icons/Ionicons';
+import Carousel from '../../common/components/Carousel';
+
+/**
+ * Channel Rewards Carousel
+ */
+@observer
+export default class RewardsCarousel extends Component {
+
+  /**
+   * Render component
+   */
+  render() {
+    const rewards = this.props.rewards;
+    const styles  = this.props.styles;
+
+    // get screen width
+    const width = Dimensions.get('window').width;
+
+    const rewardsArray = [];
+
+    if (rewards.money) {
+      rewards.money.map((rew) => {
+        rewardsArray.push(
+          <View key={`rewards${rew.amount}`} style={styles.carouselitems}>
+            <View>
+              <Text style={styles.rewardamount}>${rew.amount}+</Text>
+              <Text style={styles.rewarddesc}>{rew.description}</Text>
+            </View>
+            <Icon style={styles.rewardicon} name={'ios-flash'} size={36} />
+          </View>
+        );
+      });
+    }
+
+    return (
+      <Carousel style={styles.carousel} width={width} height={70} color={'#0071ff'}>
+        {rewardsArray}
+      </Carousel>
+    )
+  }
+}
