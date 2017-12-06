38041d9453c598da057961fb379515dce7f87abb
Martin Santangelo <msantang78@gmail.com>
2017-12-05 23:01:02 -0300

    (refactor) Discovery screen to use mobx store

--- src/discovery/DiscoveryService.js
+++ src/discovery/DiscoveryService.js
@@ -1,50 +1,21 @@
-import { MINDS_URI } from '../config/Config';
+import api from './../common/services/api.service';
 
-import {
-  AsyncStorage
-} from 'react-native';
 
-async function getAccessToken() {
-  return await AsyncStorage.getItem('@Minds:access_token');
-}
+class DiscoveryService {
 
-async function get(offset) {
-  var headers = new Headers();
-
-  const access_token = await getAccessToken();
-
-  if (!offset)
-    offset = '';
-  console.log('loading from ' + offset)
-  return new Promise((resolve, reject) => {
-    fetch(MINDS_URI + `api/v1/entities/featured/images/?access_token=${access_token}&limit=15&offset=${offset}`)
-      .then((resp) => {
-        if (resp.status == 200) {
-          resp.json()
-            .then(data => {
-              resolve(data);
-            });
-        } else {
-          reject(resp.status);
+  async getFeed(offset) {
+    return api.get('api/v1/entities/featured/images/', { limit: 15, offset: offset })
+      .then((data) => {
+        return {
+          entities: data.entities,
+          offset: data['load-next'],
         }
       })
       .catch(err => {
         console.log('error');
-        reject(err);
+        throw "Ooops";
       })
-  });
+  }
 }
 
-export function getFeed(offset) {
-  return get(offset)
-    .then((data) => {
-      return { 
-        entities: data.entities,
-        offset: data['load-next'],
-      }
-    })
-    .catch(err => {
-      console.log('error');
-      throw "Ooops";
-    })
-}
+export default new DiscoveryService();
---
+++ src/discovery/DiscoveryStore.js
@@ -0,0 +1,52 @@
+import { observable, action } from 'mobx';
+import discoveryService from './DiscoveryService';
+
+// TODO: all store are too similar make a common to be DRY
+class DiscoveryStore {
+  @observable.shallow entities = [];
+  @observable refreshing = false;
+
+  offset = '';
+
+  /**
+   * Load feed
+   */
+  loadFeed() {
+    return discoveryService.getFeed(this.offset)
+      .then(feed => {
+        this.setFeed(feed);
+      })
+      .catch(err => {
+        console.log('error', err);
+      });
+  }
+
+  @action
+  setFeed(feed) {
+    if (feed.entities) {
+      feed.entities.forEach(element => {
+        this.entities.push(element);
+      });
+    }
+    this.offset = feed.offset;
+  }
+
+  @action
+  clearFeed() {
+    this.entities = [];
+    this.offset   = '';
+  }
+
+  @action
+  refresh() {
+    this.refreshing = true;
+    this.entities   = [];
+    this.offset     = ''
+    this.loadFeed()
+      .finally(action(() => {
+        this.refreshing = false;
+      }));
+  }
+}
+
+export default new DiscoveryStore();
