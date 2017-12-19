559d90d7b7a4170190ca2efe5b04f0470c589605
Martin Santangelo <msantang78@gmail.com>
2017-12-19 14:40:16 -0300

    (feat) common styles

---
+++ src/styles/Components.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+import colors from './Colors';
+
+/**
+ * Styles
+ */
+export const ComponentsStyle = StyleSheet.create({
+
+  passwordinput: {
+    backgroundColor: '#CCC',
+    borderRadius: 5
+  }
+});
