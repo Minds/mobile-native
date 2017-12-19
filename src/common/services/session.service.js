ee08d44d9a53b219cf04ee4e61f9607eafecadcc
Martin Santangelo <msantang78@gmail.com>
2017-12-19 14:40:45 -0300

    (feat) messenger unlock

--- src/common/services/session.service.js
+++ src/common/services/session.service.js
@@ -1,25 +1,34 @@
 import { AsyncStorage } from 'react-native';
 
-const namespace = '@Minds:access_token';
+const namespace = '@Minds:';
 
 class SessionService {
 
   async getAccessToken() {
     try {
-      const token = await AsyncStorage.getItem(namespace);
+      const token = await AsyncStorage.getItem(namespace + 'access_token');
       return token;
     } catch(err) {
       return null;
     }
   }
 
+  getPrivateKey() {
+    return AsyncStorage.getItem(namespace + 'private_key');
+  }
+
+  setPrivateKey(privateKey) {
+    AsyncStorage.setItem(namespace + 'private_key', privateKey);
+  }
+
   async setAccessToken(access_token) {
-    await AsyncStorage.setItem(namespace, access_token);
+    await AsyncStorage.setItem(namespace + 'access_token', access_token);
     return true;
   }
 
   async clear() {
-    await AsyncStorage.removeItem(namespace);
+    await AsyncStorage.removeItem(namespace + 'access_token');
+    await AsyncStorage.removeItem(namespace + 'private_key');
     // TODO: navigate to login
   }
 
---
+++ src/messenger/MessengerSetup.js
@@ -0,0 +1,68 @@
+import React, {
+  Component
+} from 'react';
+
+import {
+  Text,
+  View,
+  TextInput,
+  Button
+} from 'react-native';
+
+import {
+  inject,
+  observer
+} from 'mobx-react/native'
+
+import { CommonStyle } from '../styles/Common';
+import { ComponentsStyle } from '../styles/Components';
+import CenteredLoading from '../common/components/CenteredLoading';
+/**
+ * Messenger setup
+ */
+@inject('messengerList')
+@observer
+export default class MessengerSetup extends Component {
+
+  /**
+   * password
+   * (don't use state to prevent render the component when password change)
+   */
+  password = '';
+
+  unlock = () => {
+    this.props.messengerList.getCrytoKeys(this.password);
+  }
+
+  /**
+   * Render
+   */
+  render() {
+    const unlocking = this.props.messengerList.unlocking;
+
+    const button = (unlocking) ? <CenteredLoading /> : <Button title="UNLOCK" onPress={this.unlock} />
+
+    return (
+      <View style={[CommonStyle.flexcontainer, CommonStyle.backgroundLight]}>
+        <TextInput
+          ref='password'
+          style={ComponentsStyle.passwordinput}
+          editable={true}
+          underlineColorAndroid='transparent'
+          placeholder='password...'
+          secureTextEntry={true}
+          onChangeText={(password) => this.password = password}
+        />
+        <View style={CommonStyle.marginTop2x}>
+          {button}
+        </View>
+        <View style={CommonStyle.marginTop2x}>
+          <Text style={CommonStyle.fontS}>· You only need to enter this encryption password once as long as you stay signed in.</Text>
+          <Text style={CommonStyle.fontS}>· It is important so that no one other than you and the people you are communicating with can access the content of your messages.</Text>
+          <Text style={CommonStyle.fontS}>· By default the content of your messages is fully encrypted. For heightened security you may wish to go to your app settings and turn off push notifications in order to disallow metadata from being tracked.</Text>
+          <Text style={CommonStyle.fontS}>· By default the content of your messages is fully encrypted. For heightened security you may wish to go to your app settings and turn off push notifications in order to disallow metadata from being tracked.</Text>
+        </View>
+      </View>
+    )
+  }
+}
