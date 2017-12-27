6aadcd3381f2422c3ef5bf6cb04237d88becd363
Martin Santangelo <msantang78@gmail.com>
2017-12-27 00:46:44 -0300

    (feat) reusable toolbar

---
+++ src/common/components/toolbar/Toolbar.js
@@ -0,0 +1,88 @@
+import React, { PureComponent } from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+import ToolbarItem from './ToolbarItem';
+
+
+export default class Toolbar extends PureComponent {
+
+  state = {
+    selected: null
+  }
+
+  componentWillMount() {
+    const initial = this.props.initial;
+
+    if (initial) this.state.selected = initial;
+  }
+
+  /**
+   * change state
+   */
+  changeState = (val) => {
+    if (this.state.selected == val) return;
+    this.setState({selected: val});
+
+    const onChange = this.props.onChange;
+    if (onChange) {
+      onChange(val);
+    }
+  }
+
+  /**
+   * Render
+   */
+  render() {
+    options = this.props.options;
+
+
+    // buttons
+    const buttons = options.map(data => {
+      return (
+        <ToolbarItem
+          text={data.text}
+          icon={data.icon}
+          key={data.value}
+          value={data.value}
+          iconType={data.iconType}
+          selected={data.value == this.state.selected}
+          onPress={this.changeState}
+        />
+      )
+    });
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.topbar}>
+        {buttons}
+        </View>
+      </View>
+    );
+  }
+}
+
+const selectedcolor = '#0071ff';
+const color = '#444'
+
+const styles = StyleSheet.create({
+  container: {
+    height: 65,
+    display: 'flex',
+    flexDirection: 'row',
+    paddingTop: 12,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#EEE',
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopColor: '#EEE',
+  },
+  topbar: {
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+});
