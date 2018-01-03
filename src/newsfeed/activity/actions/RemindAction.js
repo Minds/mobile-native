3e806508c5b1663490a49676aeb3f73edf28d42a
Martin Santangelo <msantang78@gmail.com>
2018-01-03 14:22:29 -0300

    (refactor) split actions in smaller components

---
+++ src/newsfeed/activity/actions/RemindAction.js
@@ -0,0 +1,89 @@
+import React, {
+  PureComponent
+} from 'react';
+
+import {
+  Text,
+  StyleSheet,
+  Modal,
+  View
+} from 'react-native';
+
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import { Icon } from 'react-native-elements';
+import Remind from '../remind/Remind';
+import { CommonStyle } from '../../../styles/Common';
+
+/**
+ * Remind Action Component
+ */
+export default class RemindAction extends PureComponent {
+
+  state = {
+    remindModalVisible: false,
+  }
+
+  /**
+   * Render
+   */
+  render() {
+    return (
+      <View onPress={this.remind} style={CommonStyle.flexContainer}>
+        <Icon onPress={this.remind} color={this.props.entity['reminds'] > 0 ? 'rgb(70, 144, 214)' : 'rgb(96, 125, 139)'} name='repeat' size={20} />
+        <Text onPress={this.remind} style={styles.actionIconText}>{this.props.entity['reminds'] > 0 ? this.props.entity['reminds'] : ''}</Text>
+        <View style={styles.modalContainer}>
+          <Modal animationType={"slide"} transparent={false}
+            visible={this.state.remindModalVisible}
+            onRequestClose={this.closeRemind}>
+            <View style={styles.modal}>
+              <View style={styles.modalHeader}>
+                <IonIcon onPress={this.closeRemind} color='gray' size={30} name='md-close' />
+              </View>
+              <Remind entity={this.props.entity} />
+            </View>
+          </Modal>
+        </View>
+      </View>
+    )
+  }
+
+  /**
+   * Close remind modal
+   */
+  closeRemind = () => {
+    this.setState({ remindModalVisible: false });
+  }
+
+  /**
+   * Open remind modal
+   */
+  remind = () => {
+    this.setState({ remindModalVisible: true });
+  }
+}
+
+const styles = StyleSheet.create({
+  actionIconText: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    left: 52,
+    alignContent: 'center',
+    justifyContent: 'center'
+  },
+  modal: {
+    flex: 1,
+    paddingTop: 4,
+  },
+  modalContainer: {
+    alignItems: 'center',
+    backgroundColor: '#ede3f2',
+  },
+  modalHeader: {
+    padding: 5
+  }
+});
+
+
+
+
