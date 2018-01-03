87b84650b43fecacf3681d6ddefc76a6e3e0e2ec
Martin Santangelo <msantang78@gmail.com>
2018-01-03 16:40:39 -0300

    (feat) added actions and fix styles on blog

--- src/blogs/BlogsViewScreen.js
+++ src/blogs/BlogsViewScreen.js
@@ -9,12 +9,24 @@ import {
   ActivityIndicator,
 } from 'react-native';
 
+
+import { inject } from 'mobx-react/native';
+
 import FastImage from 'react-native-fast-image';
 import { Icon } from 'react-native-elements'
 import AutoHeightWebView from '../common/components/AutoHeightWebView';
+import OwnerBlock from '../newsfeed/activity/OwnerBlock';
+import formatDate from '../common/helpers/date';
+import { CommonStyle } from '../styles/Common';
+
+import ThumbUpAction from '../newsfeed/activity/actions/ThumbUpAction';
+import ThumbDownAction from '../newsfeed/activity/actions/ThumbDownAction';
+import RemindAction from '../newsfeed/activity/actions/RemindAction';
+import CommentsAction from '../newsfeed/activity/actions/CommentsAction';
 /**
  * Blog View Screen
  */
+@inject('user')
 export default class BlogsViewScreen extends Component {
 
   /**
@@ -30,10 +42,27 @@ export default class BlogsViewScreen extends Component {
   render() {
     const blog = this.props.navigation.state.params.blog;
     const image = { uri: blog.thumbnail_src };
+
+    console.log(blog)
+
+    const actions = (
+      <View style={[CommonStyle.flexContainer, CommonStyle.paddingLeft2x]}>
+        <View style={styles.container}>
+          <RemindAction entity={blog} />
+          <ThumbUpAction entity={blog} me={this.props.user.me} />
+          <ThumbDownAction entity={blog} me={this.props.user.me} />
+          <CommentsAction entity={blog} navigation={this.props.navigation} />
+        </View>
+      </View>
+    )
+
     return (
       <ScrollView style={styles.screen}>
         <FastImage source={image} resizeMode={FastImage.resizeMode.cover} style={styles.image} />
         <Text style={styles.title}>{blog.title}</Text>
+        <OwnerBlock entity={blog} navigation={this.props.navigation} rightToolbar={actions}>
+          <Text style={styles.timestamp}>{formatDate(blog.time_created)}</Text>
+        </OwnerBlock>
         <View style={styles.description}>
           <AutoHeightWebView html={blog.description}/>
         </View>
@@ -54,6 +83,12 @@ const styles = {
     height: 40,
     width: 40,
   },
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 4
+  },
   description: {
     paddingLeft: 10,
     paddingRight: 10,
@@ -63,6 +98,10 @@ const styles = {
   },
   image: {
     height: 200
+  },
+  timestamp: {
+    fontSize: 11,
+    color: '#888',
   },
   title: {
     paddingTop: 10,
