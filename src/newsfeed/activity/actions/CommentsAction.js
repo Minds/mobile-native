8e6bc644866a26299c58ae9e08eebdd09807c674
Martin Santangelo <msantang78@gmail.com>
2018-01-19 14:26:07 -0300

    (feat) activity comments counter link

--- src/newsfeed/activity/actions/CommentsAction.js
+++ src/newsfeed/activity/actions/CommentsAction.js
@@ -24,7 +24,7 @@ export default class CommentsAction extends PureComponent {
     return (
       <View style={[CommonStyle.flexContainer, CommonStyle.rowJustifyCenter]}>
         <Icon color={this.props.entity['comments:count'] > 0 ? 'rgb(70, 144, 214)' : 'rgb(96, 125, 139)'} name='chat-bubble' size={20} onPress={this.openComments} />
-        <Text style={CommonStyle.paddingLeft}>{this.props.entity['comments:count'] > 0 ? this.props.entity['comments:count'] : ''}</Text>
+        <Text style={CommonStyle.paddingLeft} onPress={this.openComments}>{this.props.entity['comments:count'] > 0 ? this.props.entity['comments:count'] : ''}</Text>
       </View>
     );
   }
