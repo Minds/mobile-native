8856da3add22124c1cabe060b300f26a8a2acf8d
Martin Alejandro Santangelo <martin@minds.com>
2018-03-16 13:40:05 -0300

    (feat) reimplement read more on post text (#389)

--- src/common/components/explicit/ExplicitText.js
+++ src/common/components/explicit/ExplicitText.js
@@ -7,12 +7,12 @@ import {
 } from 'react-navigation';
 
 import {observer} from "mobx-react/native";
-import ReadMore from 'react-native-read-more-text';
 import entities from 'entities';
 import {
   Text,
   StyleSheet,
   View,
+  Dimensions
 } from 'react-native';
 
 import Tags from '../../../common/components/Tags';
@@ -21,51 +21,109 @@ import colors from '../../../styles/Colors';
 
 @observer
 export default class ExplicitText extends Component {
+  needTruncate = false;
 
+  state = {
+    more: false,
+    width: Dimensions.get('window').width
+  }
+
+  /**
+   * Handle dimensions changes
+   */
+  dimensionChange = () => {
+    this.setState({
+      width: Dimensions.get('window').width
+    });
+  }
+
+  /**
+   * On component will mount
+   */
+  componentWillMount() {
+    Dimensions.addEventListener("change", this.dimensionChange);
+  }
+
+  /**
+   * On component will unmount
+   */
+  componentWillUnmount() {
+    // allways remove listeners on unmount
+    Dimensions.removeEventListener("change", this.dimensionChange);
+  }
+
+  /**
+   * Render
+   */
   render() {
     const entity = this.props.entity;
-    const message = entities.decodeHTML(this.props.entity.text).trim();
+    let message = entities.decodeHTML(this.props.entity.text).trim();
+    
+    let body = null;
+    let moreLess = null;
+    
+    if (message != '') {
+      const truncated = this.truncate(message);
+      // truncate if necessary
+      if (message.length != truncated.length) {
+        if (!this.state.more) message = truncated;
+        moreLess = this.getMoreLess();
+      }
 
-    let body;
-
-    if (message == '') {
-      body = null;
-    } else {
-      body = 
-
-      body = <ReadMore
-        numberOfLines={4}
-        renderTruncatedFooter={this._renderTruncatedFooter}
-        renderRevealedFooter={this._renderRevealedFooter}
-      >
-        { entity.mature ? 
+      body = entity.mature ? 
           <Text style={styles.mature}>{message}</Text> :
-          <Tags navigation={this.props.navigation} style={this.props.style}>{message}</Tags> }
-      </ReadMore>
+          <Tags navigation={this.props.navigation} style={this.props.style}>{message}</Tags>
     }
 
-
     return (
-        <View style={styles.container}>
-            { body }
-        </View>
+      <View style={styles.container}>
+        { body }
+        { moreLess }
+      </View>
     );
   }
 
-  _renderTruncatedFooter = (handlePress) => {
-    return (
-      <Text style={styles.readmore} onPress={handlePress}>
-        Read more
-      </Text>
-    );
+  /**
+   * Truncate text
+   * @param {string} message 
+   */
+  truncate(message) {
+    const limit = this.getTextLimit();
+
+    let truncated = false;
+    let lines = message.split('\n');
+    if (lines.length > 4) {
+      lines = lines.slice(0,4);
+      truncated = true;
+    }
+
+    lines = lines.join('\n');
+    if (lines.length > limit) {
+      return lines.substr(0, limit) + '...';
+    }
+    return lines + (truncated ? '...':'');
   }
 
-  _renderRevealedFooter = (handlePress) => {
-    return (
-      <Text style={styles.readmore} onPress={handlePress}>
-        Show less
-      </Text>
-    );
+  /**
+   * Get text char limit based on screen width
+   */
+  getTextLimit() {
+    return this.state.width / 2;
+  }
+
+  /**
+   * Returns more or less button
+   */
+  getMoreLess() {
+    const msg = (this.state.more) ? 'Show less' : 'Read More'; 
+    return <Text style={styles.readmore} onPress={this.toggleReadMore}>{msg}</Text>
+  }
+
+  /**
+   * Toggle read more
+   */
+  toggleReadMore = () => {
+    this.setState({more: !this.state.more});
   }
 }
 
