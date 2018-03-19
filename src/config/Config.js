dcfa4471ce986fe4a299703eaf2084bfe331f97d
Mark Harding <mark@kramnorth.com>
2018-03-19 14:43:43 -0400

    (chore): use cdn.minds.com and mobile.minds.com

--- src/config/Config.js
+++ src/config/Config.js
@@ -6,7 +6,7 @@ import {
 // export const MINDS_URI = 'http://dev.minds.io/';
 
 // remember to update deeplink uri on AndroidManifest.xml !!!
-export const MINDS_URI = 'https://walrus.minds.com/';
+export const MINDS_URI = 'https://mobile.minds.com/';
 
 export const MINDS_URI_SETTINGS = {
   basicAuth: 'crypto:ohms',
@@ -14,7 +14,7 @@ export const MINDS_URI_SETTINGS = {
 
 export const SOCKET_URI = 'wss://ha-socket-io-us-east-1.minds.com:3030'
 
-export const MINDS_CDN_URI = 'https://d3ae0shxev0cb7.cloudfront.net/';
+export const MINDS_CDN_URI = 'https://cdn.minds.com/';
 // export const MINDS_CDN_URI = 'http://dev.minds.io/';
 
 export const BLOCKCHAIN_URI = 'https://rinkeby.infura.io/';
