a4961bacd36bdf27187a803e6572920d36463200
Mark Harding <mark@kramnorth.com>
2018-03-22 09:51:40 -0400

    (chore): use onserbable for loading

--- src/discovery/DiscoveryStore.js
+++ src/discovery/DiscoveryStore.js
@@ -24,7 +24,7 @@ class DiscoveryStore {
   @observable type       = 'object/image';
   @observable category   = 'all';
 
-  loading = false;
+  @observable loading = false;
 
   constructor() {
     this.buildListStores();
