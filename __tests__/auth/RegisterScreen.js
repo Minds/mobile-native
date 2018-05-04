aacc73904a7f066f7eaf81ead666ad57cb847a75
Martin Alejandro Santangelo <martin@minds.com>
2018-05-04 11:00:38 -0300

    (chore) register screen spec test (#517)

---
+++ __tests__/auth/RegisterScreen.js
@@ -0,0 +1,73 @@
+import 'react-native';
+import React from 'react';
+import { Text, TouchableOpacity } from "react-native";
+import { shallow } from 'enzyme';
+
+import RegisterScreen from '../../src/auth/RegisterScreen';
+
+jest.mock('../../src/auth/RegisterForm', () => 'RegisterForm');
+jest.mock('../../src/common/components/VideoBackground', () => 'VideoBackground');
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+describe('RegisterScreen component', () => {
+  it('should renders correctly', () => {
+    const loginScreen = renderer.create(
+      <RegisterScreen />
+    ).toJSON();
+    expect(loginScreen).toMatchSnapshot();
+  });
+
+  it('should shows register form component', async () => {
+    const wrapper = shallow(
+      <RegisterScreen />
+    );
+
+    // find register form
+    let render = wrapper.dive();
+    const registerForms = render.find('RegisterForm');
+
+    // should contain 1 login form
+    expect(registerForms.length).toBe(1);
+  });
+
+  it('should navigate on RegisterForm events', async () => {
+
+    const navigation = {
+      dispatch: jest.fn()
+    };
+
+    const wrapper = shallow(
+      <RegisterScreen navigation={navigation}/>
+    );
+
+    // find register form
+    let render = wrapper.dive();
+    const registerForms = render.find('RegisterForm');
+
+    // should contain 1 login form
+    expect(registerForms.length).toBe(1);
+
+    // simulate onRegister event
+    registerForms.at(0).props().onRegister();
+
+    // should call navigate
+    expect(navigation.dispatch.mock.calls.length).toEqual(1);
+
+    // with a navigation action with route to OnboardingScreen
+    expect(navigation.dispatch.mock.calls[0][0].actions[0].routeName).toEqual('OnboardingScreen');
+
+    // clear the mock
+    navigation.dispatch.mockClear();
+
+    // simulate onBack event
+    registerForms.at(0).props().onBack();
+
+    // should call navigate
+    expect(navigation.dispatch.mock.calls.length).toEqual(1);
+
+    // with a navigation action with route to Login
+    expect(navigation.dispatch.mock.calls[0][0].actions[0].routeName).toEqual('Login');
+  });
+});
