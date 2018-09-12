418c243954408d7f6a351c798117f6eb6ffc0536
Martin Alejandro Santangelo <martin@minds.com>
2018-09-12 12:37:37 -0500

    [Sprint/World] (fix) android build (#653)
    * (fix) android build
    
    * (fix) release mode babel plugins issue

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
 
-AppRegistry.registerComponent('Minds', () => App);
+import applyDecoratedDescriptor from '@babel/runtime/helpers/es6/applyDecoratedDescriptor';
+import initializerDefineProperty from '@babel/runtime/helpers/es6/initializerDefineProperty';
+Object.assign(babelHelpers, {
+  applyDecoratedDescriptor,
+  initializerDefineProperty,
+});
+
+// import App from './App';
+// RN 0.56 Release version crashes
+// Workaround: RN 0.56 Release version crashes
+// Sources:
+//      https://github.com/facebook/react-native/issues/19827
+//      https://github.com/facebook/react-native/issues/20150
+AppRegistry.registerComponent('Minds', () => require('./App').default);
