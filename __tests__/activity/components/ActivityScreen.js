76a921e6631a662622bcb67c8dafaa4a01b5feff
Martin Santangelo <msantang78@gmail.com>
2018-12-20 22:06:23 -0300

    (chore) improve activity screen spec tests

--- __tests__/activity/components/ActivityScreen.js
+++ __tests__/activity/components/ActivityScreen.js
@@ -7,36 +7,26 @@ import {
 } from 'react-native';
 
 import ActivityScreen from '../../../src/newsfeed/ActivityScreen';
-import UserStore from '../../../src/auth/UserStore';
-import SingleEntityStore from '../../../src/common/stores/SingleEntityStore';
+// import SingleEntityStore from '../../../src/common/stores/SingleEntityStore';
 import RichEmbedStore from '../../../src/common/stores/RichEmbedStore';
 import commentsStoreProvider from '../../../src/comments/CommentsStoreProvider';
 
 import { commentsServiceFaker } from '../../../__mocks__/fake/CommentsFaker';
 import { activitiesServiceFaker } from '../../../__mocks__/fake/ActivitiesFaker';
 import CommentList from '../../../src/comments/CommentList';
+jest.mock('../../../src/newsfeed/NewsfeedService');
+import { getSingle } from '../../../src/newsfeed/NewsfeedService';
 
 jest.mock('../../../src/newsfeed/activity/Activity', () => 'Activity');
 jest.mock('../../../src/comments/CommentList', () => 'CommentList');
 jest.mock('../../../src/common/components/CenteredLoading', () => 'CenteredLoading');
-jest.mock('../../../src/auth/UserStore');
-
-// jest.mock('../../../src/comments/CommentModel');
 jest.mock('../../../src/comments/CommentsStore');
 jest.mock('../../../src/comments/CommentsStoreProvider');
-
 
 describe('Activity screen component', () => {
 
   let user, comments, entity, screen, navigation;
   beforeEach(() => {
-    navigation = {
-      push: jest.fn(),
-      state: {
-        routeName: 'some',
-        params: {entity: activitiesServiceFaker().load(1).activities[0]}
-      }
-    };
     let mockResponse = commentsServiceFaker().load(5);
     commentsStoreProvider.get.mockReturnValue({
       comments: mockResponse.comments,
@@ -50,19 +40,51 @@ describe('Activity screen component', () => {
       },
       attachment:{}
     });
-    user = new UserStore();
-    screen = shallow(
-      <ActivityScreen user={user} navigation={navigation}/>
-    );
 
     jest.runAllTimers();
   });
 
-  it('renders correctly', async () => {
+  it('renders correctly with an entity as param', async () => {
+    navigation = {
+      push: jest.fn(),
+      state: {
+        routeName: 'some',
+        params: {entity: activitiesServiceFaker().load(1).activities[0]}
+      }
+    };
+    screen = shallow(
+      <ActivityScreen navigation={navigation}/>
+    );
     expect(screen).toMatchSnapshot();
+
+    // should have a comment list component
+    expect(screen.find(CommentList)).toHaveLength(1);
   });
 
-  it('should have a CommentList', async () => {
-    expect(screen.find(CommentList)).toHaveLength(1);
+  it('should show loader until it loads the activity', async (done) => {
+    navigation = {
+      push: jest.fn(),
+      state: {
+        routeName: 'some',
+        params: {guid: '1'}
+      }
+    };
+
+    getSingle.mockResolvedValue(activitiesServiceFaker().load(1).activities[0]);
+
+    screen = shallow(
+      <ActivityScreen navigation={navigation}/>
+    );
+    // shoul show loading
+    expect(screen).toMatchSnapshot();
+
+    screen.update();
+
+    // workaround to run after the async didmount
+    setImmediate(() => {
+      // should show the activity
+      expect(screen).toMatchSnapshot();
+      done();
+    });
   });
 });
