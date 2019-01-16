953cebd426c990bf7e37a9d1e4934a01e08e7f67
Martin Santangelo <msantang78@gmail.com>
2019-01-16 14:14:40 -0300

    (chore) update spec tests snapshots

--- __tests__/common/services/features.service.js
+++ __tests__/common/services/features.service.js
@@ -7,7 +7,7 @@ import { MINDS_FEATURES } from '../../../src/config/Config';
 describe('Feature service', () => {
 
   it('should return features', async () => {
-    let features = {"crypto": false, "legacy": false, "monetization": true, "suggested_blogs_screen": false, "suggested_discovery_screen": false, "suggested_groups_screen": false}
+    let features = {"crypto": false, "legacy": false, "monetization": true, "suggested_blogs_screen": true, "suggested_discovery_screen": false, "suggested_groups_screen": true}
 
     expect(service.features).toEqual(features);
     expect(service.has('crypto')).toEqual(false);
