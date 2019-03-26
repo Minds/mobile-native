11b7c5fb4f64cfdf91bf2ae2c2686d9fd1059093
Martin Santangelo <msantang78@gmail.com>
2019-03-26 18:33:11 -0300

    (feat) separate jest configs

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const config = {
+  "automock": false,
+  "cacheDirectory": ".jest/cache",
+  "testRegex": "./__tests__/.*.js$",
+  "preset": "react-native",
+  "setupFiles": [
+    "<rootDir>/setupTests.js"
+  ],
+  "testPathIgnorePatterns": [
+    "/node_modules/"
+  ],
+  "transformIgnorePatterns": [
+    "node_modules/(?!react-native|react-navigation)/"
+  ],
+  "moduleNameMapper": {
+    "^image![a-zA-Z0-9$_-]+$": "GlobalImageStub",
+    "^[@./a-zA-Z0-9$_-]+\\.(png|gif)$": "RelativeImageStub"
+  },
+  "snapshotSerializers": [
+    "./node_modules/enzyme-to-json/serializer"
+  ]
+}
+
+module.exports = config
---
+++ jest.e2e.config.js
@@ -0,0 +1,21 @@
+const config = {
+  "automock": false,
+  "cacheDirectory": ".jest/cache",
+  "testRegex": "./__e2e__/.*.js$",
+  "preset": "react-native",
+  "testPathIgnorePatterns": [
+    "/node_modules/"
+  ],
+  "transformIgnorePatterns": [
+    "node_modules/(?!react-native|react-navigation|vargs)/"
+  ],
+  "moduleNameMapper": {
+    "^image![a-zA-Z0-9$_-]+$": "GlobalImageStub",
+    "^[@./a-zA-Z0-9$_-]+\\.(png|gif)$": "RelativeImageStub"
+  },
+  "snapshotSerializers": [
+    "./node_modules/enzyme-to-json/serializer"
+  ]
+}
+
+module.exports = config
