02458448850127978728589aa7483fc9ef9fef73
Martin Santangelo <msantang78@gmail.com>
2019-03-26 21:59:27 -0300

    (fix) e2e selectors inconsistencies between platforms

---
+++ src/common/helpers/testID.js
@@ -0,0 +1,9 @@
+import { Platform } from 'react-native';
+
+export default function(name) {
+  if (Platform.OS === 'ios') {
+    return {testID: name};
+  } else {
+    return {accessibilityLabel: name}
+  }
+}
