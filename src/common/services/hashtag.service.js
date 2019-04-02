3bd1d12598ef814c7da052a17a3eaf79785f82fc
Ben Hayward <ben@minds.com>
2019-04-02 14:08:11 +0000

    [Sprint/Votes]: Maximum of 5 hashtags allowed in post or edit

--- src/common/services/hashtag.service.js
+++ src/common/services/hashtag.service.js
@@ -6,6 +6,7 @@ import { has } from 'mobx';
  */
 class HashtagService {
 
+  maxHashtags = 5;
   /**
    * Get suggested hastags
    */
@@ -29,6 +30,30 @@ class HashtagService {
   add(hashtag) {
     return api.post(`api/v2/hashtags/user/${hashtag}`);
   }
+
+  /**
+   * Gets a list of all hashtags in a given string. 
+   * For example "cat #dog bat #rat returns ['dog','rat']")
+   * 
+   * @input {string} inputText - The text to be searched.
+   * @returns {Array<string>} - array of each hashtag minus the # (as a string).
+
+   *  Credit to Arnaud Valensi - http://geekcoder.org/js-extract-hashtags-from-text/
+   */
+  slice(input) {
+    try{
+      const regex = /(?:^|\s)(?:#)([a-zA-Z\d]+)/gm;
+      let matches = [];
+      let match;
+      while ((match = regex.exec(input))) {
+          matches.push(match[1]);
+      }
+      return matches;
+    }catch(e){
+      return [];
+    }
+  }
+
 }
 
 export default new HashtagService();
