8f5b060bf81aeaf4f89d1eb5eea7dcf9fcd9fd32
Juan Manuel Solaro <juanmsolaro@gmail.com>
2019-09-30 12:14:45 -0300

    (fix) revert changes on blogsService

--- src/blogs/BlogsService.js
+++ src/blogs/BlogsService.js
@@ -1,6 +1,5 @@
 import api from './../common/services/api.service';
 import stores from '../../AppStores';
-import FeedsService from './../common/services/feeds.service';
 
 /**
  * Blogs Service
@@ -11,22 +10,11 @@ class BlogsService {
    * Load Blogs
    */
   async loadList(filter, offset) {
-    let endpoint = '';
-    let oldBlogsApi = 'api/v1/blog/' + filter;
-    if (filter != 'network') {
-      const feedsService = new FeedsService();
-      feedsService.setEndpoint('api/v2/feeds/container/968187695744425997/blogs');
-      await feedsService.fetchRemoteOrLocal();
-      const entities = await feedsService.getEntities();
-      return {
-        entities: entities || [],
-        offset: '',
-      };
-    } else {
-      endpoint = (filter === 'suggested') ?
-      'api/v2/entities/suggested/blogs' + (stores.hashtag.all ? '/all' : '' ) :
-      oldBlogsApi;
-    }
+
+    let endpoint = (filter === 'suggested') ?
+    'api/v2/entities/suggested/blogs' + (stores.hashtag.all ? '/all' : '' ) :
+    'api/v1/blog/' + filter;
+
     const data = await api.get(endpoint, { limit: 12, offset: offset });
     return {
       entities: data.entities || [],
