7536cdc29516b1fdce133e89da85a7641c837316
Juan Manuel Solaro <juanmsolaro@gmail.com>
2019-09-29 22:17:02 -0300

    (chore) change blogs loadlist to use new api

--- src/blogs/BlogsService.js
+++ src/blogs/BlogsService.js
@@ -1,5 +1,6 @@
 import api from './../common/services/api.service';
 import stores from '../../AppStores';
+import FeedsService from './../common/services/feeds.service';
 
 /**
  * Blogs Service
@@ -10,11 +11,22 @@ class BlogsService {
    * Load Blogs
    */
   async loadList(filter, offset) {
-
-    let endpoint = (filter === 'suggested') ?
-     'api/v2/entities/suggested/blogs' + (stores.hashtag.all ? '/all' : '' ) :
-     'api/v1/blog/' + filter;
-
+    let endpoint = '';
+    let oldBlogsApi = 'api/v1/blog/' + filter;
+    if (filter != 'network') {
+      const feedsService = new FeedsService();
+      feedsService.setEndpoint('api/v2/feeds/container/968187695744425997/blogs');
+      await feedsService.fetchRemoteOrLocal();
+      const entities = await feedsService.getEntities();
+      return {
+        entities: entities || [],
+        offset: '',
+      };
+    } else {
+      endpoint = (filter === 'suggested') ?
+      'api/v2/entities/suggested/blogs' + (stores.hashtag.all ? '/all' : '' ) :
+      oldBlogsApi;
+    }
     const data = await api.get(endpoint, { limit: 12, offset: offset });
     return {
       entities: data.entities || [],
