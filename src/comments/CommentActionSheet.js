bb32f95492eb13a19feb82d4e34d301f93baccf2
Martin Santangelo <msantang78@gmail.com>
2019-10-07 10:00:18 -0300

    (feat) move comment action sheet to another component

---
+++ src/comments/CommentActionSheet.js
@@ -0,0 +1,85 @@
+import React, {
+  Component
+} from 'react';
+
+import ActionSheet from 'react-native-actionsheet';
+import i18n from '../common/services/i18n.service';
+import { FLAG_EDIT_COMMENT, FLAG_DELETE_COMMENT, FLAG_CREATE_COMMENT } from '../common/Permissions';
+
+/**
+ * Comment Component
+ */
+export default class CommentActionSheet extends Component {
+
+  state = {
+    options: [],
+  }
+
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+  }
+
+  /**
+   * Show actions
+   */
+  showActions = () => {
+    this.setState({
+      options: this.getOptions()
+    }, () => {
+      this.ref.current && this.ref.current.show();
+    });
+  }
+
+  handleSelection = (i) => {
+    const action = this.state.options[i];
+    this.props.onSelection && this.props.onSelection(action);
+  }
+
+  /**
+   * Get actionsheet options
+   */
+  getOptions = () => {
+    let actions = [i18n.t('cancel')];
+
+    const comment = this.props.comment;
+
+    if (comment.can(FLAG_EDIT_COMMENT)) {
+      actions.push( i18n.t('edit') );
+      if (!comment.mature) {
+        actions.push( i18n.t('setExplicit') );
+      } else {
+        actions.push( i18n.t('removeExplicit') );
+      }
+    }
+
+    if (comment.can(FLAG_DELETE_COMMENT)) {
+      actions.push( i18n.t('delete') );
+    }
+
+    if (comment.parent_guid_l2 == 0 && comment.can(FLAG_CREATE_COMMENT)) {
+      actions.push( i18n.t('reply') );
+    }
+
+    if (!comment.isOwner()) {
+      actions.push( i18n.t('report') );
+    }
+
+    actions.push( i18n.t('copy') );
+    return actions;
+  }
+
+  /**
+   * Render
+   */
+  render() {
+    return (
+      <ActionSheet
+        ref={this.ref}
+        options={this.state.options}
+        onPress={this.handleSelection}
+        cancelButtonIndex={0}
+      />
+    )
+  }
+}
