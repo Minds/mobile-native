8e4e62c2f1c670e479c84d434785961799284fb8
Martin Santangelo <msantang78@gmail.com>
2019-10-17 13:05:02 -0300

    (feat) test base model permissions can method

---
+++ __tests__/common/BaseModel.js
@@ -0,0 +1,33 @@
+import BaseModel from '../../src/common/BaseModel';
+import featuresService from '../../src/common/services/features.service';
+
+describe('base model', () => {
+
+  beforeEach(() => {
+
+  });
+
+  it('should set and validate permissions', () => {
+
+    featuresService.features = {permissions: true};
+
+    const data = {
+      permissions: [
+        'permissions1',
+        'permissions2',
+        'permissions3',
+      ]
+    }
+
+    const model = new BaseModel();
+    model.setPermissions(data);
+
+    // should validate permissions
+    expect(model.can('permissions1')).toBe(true);
+    expect(model.can('permissions2')).toBe(true);
+    expect(model.can('permissions3')).toBe(true);
+    expect(model.can('permissions4')).toBe(false);
+
+  });
+
+});
