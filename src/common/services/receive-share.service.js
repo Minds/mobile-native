845ac4bc8e010406eedc662e93e0b36dd2e562d4
Martin Santangelo <msantang78@gmail.com>
2019-11-05 12:01:51 -0300

    (feat) implement share to minds with new forked package

--- src/common/services/receive-share.service.js
+++ src/common/services/receive-share.service.js
@@ -1,9 +1,6 @@
 import RNFileShareIntent from 'react-native-file-share-intent';
 import navigationService from '../../navigation/NavigationService';
-import { Platform } from 'react-native';
 
-const IMAGE_PREFIX = /^image\/([a-z]|\*)*@/;
-const VIDEO_PREFIX = /^video\/([a-z]|\*)*@/;
 
 /**
  * Receive Share Service
@@ -13,16 +10,15 @@ class ReceiveShareService {
    * Handle received data
    */
   handle() {
-    // TODO: 0.61 fix the handling of content:// file paths
-    // TODO: 0.61 Implement Shared.js for ios
-    RNFileShareIntent.getFilepath(text => {
-      if (text && text !== null) {
-        if (text.match(IMAGE_PREFIX)) {
-          navigationService.navigate('Capture',{image: text.replace(IMAGE_PREFIX, '')});
-        } else if (text.match(VIDEO_PREFIX)) {
-          navigationService.navigate('Capture',{video: text.replace(VIDEO_PREFIX, '')});
+    RNFileShareIntent.getFilepath(async (text, type) => {
+      RNFileShareIntent.clearFilePath();
+      if (text && type) {
+        if (type.startsWith('video/')) {
+          navigationService.navigate('Capture', {video: text});
+        } else if (type.startsWith('image/')) {
+          navigationService.navigate('Capture', {image: text});
         } else {
-          navigationService.navigate('Capture',{text});
+          navigationService.navigate('Capture', {text});
         }
       }
     });
