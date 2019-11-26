958ae52cc21055447c66d4b7e2cb9f47987c9111
Juan Manuel Solaro <juanmsolaro@gmail.com>
2019-11-26 16:09:24 -0300

    (fix) delete firstTest

--- e2e/firstTest.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Example', () => {
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
-
-  it('should have login screen', async () => {
-    await expect(element(by.id('usernameInput'))).toBeVisible();
-  });
-});
