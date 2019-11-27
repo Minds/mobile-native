7d7de4f3e8b5dc8ba2490f3914115330a79bf228
Martin Santangelo <msantang78@gmail.com>
2019-11-27 18:46:41 -0300

    (feat) improve pulse animation with reanimated and fix ios simulator issue

--- src/common/components/Pulse.js
+++ src/common/components/Pulse.js
@@ -1,57 +1,104 @@
 import React from 'react';
-import { View, StyleSheet, Animated, Easing, Dimensions } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Animated, { Easing } from "react-native-reanimated";
+import { bInterpolate, loop } from "react-native-redash";
 
-const { height, width } = Dimensions.get('window');
+const { Value, useCode, set } = Animated;
 
-export default class Pulse extends React.Component {
-	constructor(props) {
-		super(props);
+export default function(props) {
+	const animation = new Value(0);
+  useCode(
+    set(
+      animation,
+      loop({
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.in(Easing.ease),
+      }),
+    ),
+    [animation],
+  );
+  const scale = bInterpolate(animation, 1, 1.3);
+	const opacity = bInterpolate(animation, 1, 0);
+	const pulseMaxSize = Math.round(1.3 * props.size);
 
-		this.anim = new Animated.Value(0);
-	}
-
-	componentDidMount() {
-		Animated.timing(this.anim, {
-			toValue: 1,
-			duration: this.props.interval,
-			easing: Easing.in,
-		})
-		.start();
-	}
-
-	render() {
-		const { size, pulseMaxSize, borderColor, backgroundColor, getStyle } = this.props;
-
-		return (
-			<View style={[styles.circleWrapper, {
-				width: pulseMaxSize,
-				height: pulseMaxSize,
-				marginLeft: -pulseMaxSize/2,
-				marginTop: -pulseMaxSize/2,
-			}]}>
-				<Animated.View
-					style={[styles.circle, {
-						borderColor,
-						backgroundColor,
-						width: this.anim.interpolate({
-							inputRange: [0, 1],
-							outputRange: [size, pulseMaxSize]
-						}),
-						height: this.anim.interpolate({
-							inputRange: [0, 1],
-							outputRange: [size, pulseMaxSize]
-						}),
-						borderRadius: pulseMaxSize/2,
-						opacity: this.anim.interpolate({
-							inputRange: [0, 1],
-							outputRange: [1, 0]
-						})
-					}, getStyle && getStyle(this.anim)]}
-				/>
-			</View>
-		);
-	}
+  return (
+    <View
+      style={[
+        styles.circleWrapper,
+        {
+					width: pulseMaxSize,
+					height: pulseMaxSize,
+					marginLeft: -pulseMaxSize/2,
+					marginTop: -pulseMaxSize/2,
+				}
+			]}
+		>
+      <Animated.View
+        style={{
+          transform: [{scale}],
+          backgroundColor: 'red',
+          borderRadius: props.size / 2,
+          width: props.size,
+          height: props.size,
+          opacity,
+        }}
+			/>
+      </View>
+  );
 }
+
+
+
+// export default class Pulse extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.anim = new Animated.Value(0);
+// 	}
+
+// 	componentDidMount() {
+// 		Animated.timing(this.anim, {
+// 			toValue: 1,
+// 			duration: this.props.interval,
+// 			easing: Easing.in,
+// 		})
+// 		.start();
+// 	}
+
+// 	render() {
+// 		const { size, pulseMaxSize, borderColor, backgroundColor, getStyle } = this.props;
+
+// 		return (
+// 			<View style={[styles.circleWrapper, {
+// 				width: pulseMaxSize,
+// 				height: pulseMaxSize,
+// 				marginLeft: -pulseMaxSize/2,
+// 				marginTop: -pulseMaxSize/2,
+// 			}]}>
+// 				<Animated.View
+// 					style={[styles.circle, {
+// 						borderColor,
+// 						backgroundColor,
+// 						width: this.anim.interpolate({
+// 							inputRange: [0, 1],
+// 							outputRange: [size, pulseMaxSize]
+// 						}),
+// 						height: this.anim.interpolate({
+// 							inputRange: [0, 1],
+// 							outputRange: [size, pulseMaxSize]
+// 						}),
+// 						borderRadius: pulseMaxSize/2,
+// 						opacity: this.anim.interpolate({
+// 							inputRange: [0, 1],
+// 							outputRange: [1, 0]
+// 						})
+// 					}, getStyle && getStyle(this.anim)]}
+// 				/>
+// 			</View>
+// 		);
+// 	}
+// }
 
 
 const styles = StyleSheet.create({
