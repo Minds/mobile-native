f75163c46100dd9126963d03a5cf0f2fbc6aaf96
Martin Santangelo <msantang78@gmail.com>
2020-02-26 11:22:54 -0300

    (fix) remove wire header and change close color

--- src/navigation/NavigationStack.js
+++ src/navigation/NavigationStack.js
@@ -108,7 +108,7 @@ const AppStack = function(props) {
       <AppStackNav.Screen name="BlogList" component={BlogsListScreen}/>
       <AppStackNav.Screen name="BoostConsole" component={BoostConsoleScreen} options={{title: i18n.t('boost')}}/>
       <AppStackNav.Screen name="BlogView" component={BlogsViewScreen} options={hideHeader}/>
-      <AppStackNav.Screen name="WireFab" component={FabScreen}/>
+      <AppStackNav.Screen name="WireFab" component={FabScreen} options={hideHeader}/>
       <AppStackNav.Screen name="WalletHistory" component={WalletHistoryScreen}/>
       <AppStackNav.Screen name="ViewImage" component={ViewImageScreen}/>
       <AppStackNav.Screen name="BlockchainWallet" component={BlockchainWalletScreen}/>
