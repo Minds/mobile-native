b4c73805342fc37fabdd78d40f878a8983256422
Martin Santangelo <msantang78@gmail.com>
2020-03-02 09:31:44 -0300

    (fix) activity screen error on unmount if entity is not loaded

--- src/newsfeed/ActivityScreen.js
+++ src/newsfeed/ActivityScreen.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 
-import {
-  View,
-  Text
-} from 'react-native';
-import { observer } from 'mobx-react'
+import { View, Text } from 'react-native';
+import { observer } from 'mobx-react';
 import FastImage from 'react-native-fast-image';
 
 import { CommonStyle as CS } from '../styles/Common';
@@ -25,13 +22,9 @@ import { FLAG_VIEW } from '../common/Permissions';
 export default
 @observer
 class ActivityScreen extends Component {
-
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: navigation.getParam('entity', {ownerObj:{name:''}}).ownerObj.name
-    };
-  };
-
+  /**
+   * Store
+   */
   entityStore = new SingleEntityStore();
 
   /**
@@ -53,8 +46,8 @@ class ActivityScreen extends Component {
     const params = this.props.route.params;
 
     if (params.entity && (params.entity.guid || params.entity.entity_guid)) {
-
-      const urn = 'urn:entity:' + (params.entity.guid || params.entity.entity_guid);
+      const urn =
+        'urn:entity:' + (params.entity.guid || params.entity.entity_guid);
 
       const entity = ActivityModel.checkOrCreate(params.entity);
 
@@ -86,7 +79,7 @@ class ActivityScreen extends Component {
       } else {
         params.entity._list.viewed.addViewed(
           params.entity,
-          params.entity._list.metadataService
+          params.entity._list.metadataService,
         );
       }
     }
@@ -98,7 +91,7 @@ class ActivityScreen extends Component {
   componentWillUnmount() {
     const entity = this.entityStore.entity;
 
-    if (entity._list && entity._list.metadataService) {
+    if (entity && entity._list && entity._list.metadataService) {
       entity._list.metadataService.popSource();
     }
   }
@@ -107,27 +100,36 @@ class ActivityScreen extends Component {
    * Get header
    */
   getHeader() {
-    return this.entityStore.entity ?
+    return this.entityStore.entity ? (
       <Activity
-        ref={o => this.activity = o}
-        entity={ this.entityStore.entity }
-        navigation={ this.props.navigation }
+        ref={this.setRef}
+        entity={this.entityStore.entity}
+        navigation={this.props.navigation}
         autoHeight={false}
-      /> : null;
+      />
+    ) : null;
   }
+
+  /**
+   * Set ref
+   * @param {NodeRef}
+   */
+  setRef = o => (this.activity = o);
 
   /**
    * On comment input focus
    */
   onFocus = () => {
     this.activity.pauseVideo();
-  }
+  };
 
   /**
    * Render
    */
   render() {
-    if (!this.entityStore.entity && !this.entityStore.errorLoading) return <CenteredLoading />;
+    if (!this.entityStore.entity && !this.entityStore.errorLoading) {
+      return <CenteredLoading />;
+    }
 
     if (!this.entityStore.entity.can(FLAG_VIEW, true)) {
       this.props.navigation.goBack();
@@ -136,27 +138,28 @@ class ActivityScreen extends Component {
 
     return (
       <View style={[CS.flexContainer]}>
-        {
-          !this.entityStore.errorLoading ?
-            <CommentList
-              header={this.getHeader()}
-              entity={this.entityStore.entity}
-              store={this.comments}
-              navigation={this.props.navigation}
-              onInputFocus={this.onFocus}
-              route={this.props.route}
+        {!this.entityStore.errorLoading ? (
+          <CommentList
+            header={this.getHeader()}
+            entity={this.entityStore.entity}
+            store={this.comments}
+            navigation={this.props.navigation}
+            onInputFocus={this.onFocus}
+            route={this.props.route}
+          />
+        ) : (
+          <View style={CS.flexColumnCentered}>
+            <FastImage
+              resizeMode={FastImage.resizeMode.contain}
+              style={ComponentsStyle.logo}
+              source={require('../assets/logos/logo.png')}
             />
-          :
-            <View style={CS.flexColumnCentered}>
-              <FastImage
-                resizeMode={FastImage.resizeMode.contain}
-                style={ComponentsStyle.logo}
-                source={require('../assets/logos/logo.png')}
-              />
-              <Text style={[CS.fontL, CS.colorDanger]}>{i18n.t('activity.error')}</Text>
-              <Text style={[CS.fontM]}>{i18n.t('activity.tryAgain')}</Text>
-            </View>
-        }
+            <Text style={[CS.fontL, CS.colorDanger]}>
+              {i18n.t('activity.error')}
+            </Text>
+            <Text style={[CS.fontM]}>{i18n.t('activity.tryAgain')}</Text>
+          </View>
+        )}
       </View>
     );
   }
