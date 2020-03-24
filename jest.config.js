fadd852fe70efc6aead98e9e93b961271ccbd004
Martin Santangelo <msantang78@gmail.com>
2020-03-24 10:41:13 -0300

    (feat) install typescript

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,17 @@
 const config = {
-  "automock": false,
-  "cacheDirectory": ".jest/cache",
-  "testRegex": "./__tests__/.*.js$",
-  "preset": "react-native",
-  "setupFiles": [
-    "<rootDir>/setupTests.js"
-  ],
-  "testPathIgnorePatterns": [
-    "/node_modules/"
-  ],
-  "transformIgnorePatterns": [
-    "node_modules/(?!react-native|react-navigation)/"
-  ],
-  "moduleNameMapper": {
-    "^image![a-zA-Z0-9$_-]+$": "GlobalImageStub",
+  automock: false,
+  cacheDirectory: '.jest/cache',
+  testRegex: './__tests__/.*.js$',
+  preset: 'react-native',
+  setupFiles: ['<rootDir>/setupTests.js'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ['node_modules/(?!react-native|react-navigation)/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^image![a-zA-Z0-9$_-]+$': 'GlobalImageStub',
     // "^[@./a-zA-Z0-9$_-]+\\.(png|gif)$": "RelativeImageStub"
   },
-  "snapshotSerializers": [
-    "./node_modules/enzyme-to-json/serializer"
-  ]
-}
+  snapshotSerializers: ['./node_modules/enzyme-to-json/serializer'],
+};
 
-module.exports = config
+module.exports = config;
