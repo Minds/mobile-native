f9c52963fa5b3e5a0ab80f01c3a6c0904ee3c6f0
Martin Santangelo <msantang78@gmail.com>
2020-04-24 11:57:42 -0300

    (feat) remind preview fade

--- src/compose/RemindPreview.js
+++ src/compose/RemindPreview.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, Dimensions } from 'react-native';
+import { View, Dimensions, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import BlogCard from '../blogs/BlogCard';
 import Activity from '../newsfeed/activity/Activity';
 import { useKeyboard } from '@react-native-community/hooks';
+import ThemedStyles from '../styles/ThemedStyles';
 
 const height = Dimensions.get('screen').height;
 
@@ -13,21 +15,35 @@ const height = Dimensions.get('screen').height;
  * @param {Object} props
  */
 export default function (props) {
+  const backgroundColor = ThemedStyles.getColor('primary_background');
+  const startColor = backgroundColor + '00';
+  const endColor = backgroundColor + 'FF';
   const keyboard = useKeyboard();
   const style = {
     maxHeight: height / 2 - keyboard.keyboardHeight,
     paddingHorizontal: 20,
     overflow: 'scroll', // IMPORTANT! the activities and blogs are not rendered correctly without this!
   };
-  const ShowComponent = props.entity.subtype === 'blog' ? BlogCard : Activity;
+
+  const entity = props.entity.remind_object || props.entity;
+
+  const ShowComponent = entity.subtype === 'blog' ? BlogCard : Activity;
   const navigation = useNavigation();
+
   return (
     <View style={style}>
-      <ShowComponent
-        hideTabs={true}
-        entity={props.entity}
-        navigation={navigation}
-      />
+      <ShowComponent hideTabs={true} entity={entity} navigation={navigation} />
+      <LinearGradient colors={[startColor, endColor]} style={styles.linear} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  linear: {
+    position: 'absolute',
+    bottom: 0,
+    left: 20,
+    height: '35%',
+    width: '100%',
+  },
+});
