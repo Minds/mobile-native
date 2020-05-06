5e1a8be9e324df15303e49ae2ec26e646d4f0db6
Martin Santangelo <msantang78@gmail.com>
2020-05-06 14:14:09 -0300

    (feat) bottom options pop up component

---
+++ src/common/components/BottomOptionPopup.tsx
@@ -0,0 +1,149 @@
+import React, { useRef, useEffect } from 'react';
+import BottomSheet from 'reanimated-bottom-sheet';
+import ThemedStyles from '../../styles/ThemedStyles';
+import { StyleSheet, View, Text } from 'react-native';
+
+import { observer, useLocalStore } from 'mobx-react';
+
+type PropsType = {
+  onCancel: () => void;
+  onDone: () => void;
+  title: string;
+  show?: boolean;
+  doneText: string;
+  height: number;
+  content: React.ReactNode;
+};
+
+/**
+ * Bottom options popup
+ * @example
+ * <BottomOptionPopup
+ *   height={500}
+ *   title="Some title"
+ *   show={true}
+ *   onCancel={() => console.log('canceled')}
+ *   onDone={() => console.log('done')}
+ *   content={body}
+ *   doneText="Done"
+ * />
+ */
+const BottomOptionPopup = observer((props: PropsType) => {
+  const theme = ThemedStyles.style;
+  const ref = useRef<BottomSheet>(null);
+
+  const store = useLocalStore(
+    (p) => ({
+      showing: false,
+      close() {
+        store.showing = false;
+        ref.current?.snapTo(0);
+        ref.current?.snapTo(0);
+      },
+      cancel() {
+        store.close();
+        p.onCancel();
+      },
+      open() {
+        store.showing = true;
+        ref.current?.snapTo(1);
+        ref.current?.snapTo(1);
+      },
+      done() {
+        store.close();
+        p.onDone();
+      },
+    }),
+    props,
+  );
+
+  useEffect(() => {
+    if (props.show) {
+      // called twice as a workaround
+      store.open();
+    } else {
+      // called twice as a workaround
+      store.close();
+    }
+  }, [store, props.show]);
+
+  return (
+    <>
+      {store.showing && (
+        <View style={[styles.overlay, theme.backgroundSecondary]} />
+      )}
+      <BottomSheet
+        ref={ref}
+        snapPoints={[0, props.height]}
+        onCloseEnd={store.cancel}
+        renderHeader={() => (
+          <View style={styles.headerContainer}>
+            <View
+              style={[
+                theme.backgroundSecondary,
+                theme.rowJustifySpaceBetween,
+                theme.alignEnd,
+                styles.header,
+              ]}>
+              <Text
+                onPress={store.close}
+                style={[
+                  theme.fontXL,
+                  theme.fontMedium,
+                  theme.colorSecondaryText,
+                ]}>
+                Cancel
+              </Text>
+              <Text style={[theme.fontXXL, theme.bold, theme.textCenter]}>
+                {props.title}
+              </Text>
+              <Text
+                style={[theme.fontXL, theme.fontMedium, theme.colorLink]}
+                onPress={store.done}>
+                {props.doneText}
+              </Text>
+            </View>
+          </View>
+        )}
+        renderContent={() => (
+          <View style={[styles.panel, theme.backgroundSecondary]}>
+            {props.content}
+          </View>
+        )}
+        initialSnap={0}
+        enabledContentTapInteraction={true}
+        enabledInnerScrolling={true}
+      />
+    </>
+  );
+});
+
+export default BottomOptionPopup;
+
+const styles = StyleSheet.create({
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    opacity: 0.5,
+  },
+  headerContainer: {
+    overflow: 'hidden',
+    paddingTop: 20,
+  },
+  header: {
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: -10,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 5.0,
+    elevation: 8,
+    padding: 20,
+  },
+  panel: {
+    height: '100%',
+    padding: 20,
+  },
+});
