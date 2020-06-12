1c428b9e4473bc1b8d39b62e69fd1dc1edcd9e9e
juanmsolaro <juanmsolaro@gmail.com>
2020-06-12 15:10:58 -0300

    fixes for lock

--- src/wire/v2/lock/Lock.tsx
+++ src/wire/v2/lock/Lock.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import type ActivityModel from '../../../newsfeed/ActivityModel';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import ThemedStyles from '../../../styles/ThemedStyles';
 import LockTag from './LockTag';
@@ -11,18 +11,62 @@ type PropsType = {
   navigation: any;
 };
 
+type BlockType = 'members' | 'paywall' | 'plus';
+
+const getBlockType = (urn: string): BlockType => {
+  if (!urn) {
+    return 'paywall';
+  }
+  const type = urn.split(':')[1];
+
+  // TODO how we indentify paywall if all are support-tier
+  return type === 'support-tier' ? 'members' : 'plus';
+};
+
+const getTextForBlocked = (type: BlockType) => {
+  let message = '';
+  switch (type) {
+    case 'members':
+      message = 'Become a member to view this post';
+      break;
+    case 'plus':
+      message = 'Join Plus to view this post';
+      break;
+    case 'paywall':
+      message = 'Pay too see this post';
+      break;
+  }
+  return message;
+};
+
 const Lock = observer(({ entity, navigation }: PropsType) => {
   const theme = ThemedStyles.style;
+  const hasMedia = entity.hasMedia();
+  // we don´t know yet what the data structure be like
+  const blockedType = getBlockType(entity.wire_threshold);
+  const message = getTextForBlocked(blockedType);
+  if (!hasMedia) {
+    return (
+      <View
+        style={[
+          styles.mask,
+          theme.backgroundSeparator,
+          theme.centered,
+          theme.padding2x,
+        ]}>
+        <Text style={[theme.colorWhite, theme.fontL]}>{message}</Text>
+        <LockTag type={blockedType} />
+      </View>
+    );
+  }
+
   return (
-    <View
-      style={[
-        styles.mask,
-        theme.backgroundSeparator,
-        theme.alignJustifyCenter,
-        theme.padding2x,
-      ]}>
-      <LockTag type="plus" />
-    </View>
+    <ImageBackground
+      style={styles.backgroundImage}
+      source={entity.getThumbSource()}
+      resizeMode="cover">
+      <Text style={[theme.colorWhite, theme.fontL]}>{message}</Text>
+    </ImageBackground>
   );
 });
 
@@ -33,6 +77,12 @@ const styles = StyleSheet.create({
     height: '100%',
     width: '100%',
     zIndex: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  backgroundImage: {
+    height: '100%',
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
