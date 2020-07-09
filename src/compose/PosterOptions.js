b904750bd523ff3292c37239e91615c8e7074d12
Juanmsolaro <juanmsolaro@gmail.com>
2020-07-09 13:02:32 -0300

    poster constraint for plus monetized posts

--- src/compose/PosterOptions.js
+++ src/compose/PosterOptions.js
@@ -112,7 +112,9 @@ export default observer(function (props) {
   }));
 
   const monetizeDesc = featuresService.has('plus-2020')
-    ? store.wire_threshold.support_tier?.name || ''
+    ? store.wire_threshold.support_tier?.urn
+      ? store.wire_threshold.support_tier?.name || 'Plus'
+      : ''
     : tokens
     ? `${tokens} ${i18n.t('tokens').toLowerCase()} +`
     : '';
