246053cf1f6d3af604c1114ddf2b91e84768edff
Juanmsolaro <juanmsolaro@gmail.com>
2020-08-05 10:31:48 -0300

    add full width to title view from menu item

--- src/settings/screens/TierManagementScreen.tsx
+++ src/settings/screens/TierManagementScreen.tsx
@@ -66,9 +66,11 @@ const renderTiers = (
           title: (
             <View
               style={[
+                theme.fullWidth,
                 theme.rowJustifySpaceBetween,
                 theme.paddingTop3x,
                 theme.paddingBottom3x,
+                theme.paddingLeft2x,
               ]}>
               <Text style={theme.colorPrimaryText}>{tier.name}</Text>
               <Text
@@ -81,7 +83,7 @@ const renderTiers = (
             ? checkIcon
             : transparentCheckIcon,
         }}
-        containerItemStyle={theme.backgroundPrimary}
+        containerItemStyle={[theme.backgroundPrimary]}
       />
     ));
   } else {
