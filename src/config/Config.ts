32f0d9fde72991b32bd9217e070ac8b431db1aab
Martin Santangelo <msantang78@gmail.com>
2020-09-16 14:46:54 -0300

    (feat) deeplinks exception

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -51,6 +51,8 @@ export const MINDS_FEATURES = {
  * Deeplink to screen/params maping
  */
 export const MINDS_DEEPLINK = [
+  ['token', 'Redirect'],
+  ['help', 'Redirect'],
   ['email-confirmation', 'EmailConfirmation'],
   ['groups/profile/:guid/feed', 'GroupView'],
   ['groups/profile/:guid', 'GroupView'],
