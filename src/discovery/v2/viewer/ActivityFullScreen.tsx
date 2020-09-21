884d6e936f4f4e80a0ec04c561a057a68ea3ab34
Manishoo <maanishooshtari@gmail.com>
2020-09-21 20:45:40 +0330

    Fix video auto-play on Data Saver mode

--- src/discovery/v2/viewer/ActivityFullScreen.tsx
+++ src/discovery/v2/viewer/ActivityFullScreen.tsx
@@ -10,6 +10,7 @@ import * as entities from 'entities';
 import type ActivityModel from '../../../newsfeed/ActivityModel';
 import MediaView from '../../../common/components/MediaView';
 import OwnerBlock from '../../../newsfeed/activity/OwnerBlock';
+import settingsStore from '../../../settings/SettingsStore';
 import ThemedStyles from '../../../styles/ThemedStyles';
 import ActivityActionSheet from '../../../newsfeed/activity/ActivityActionSheet';
 import formatDate from '../../../common/helpers/date';
@@ -96,7 +97,12 @@ const ActivityFullScreen = observer((props: PropsType) => {
       // if we have some video playing we pause it and reset the current video
       videoPlayerService.setCurrent(null);
 
-      if (user.plus && !user.disable_autoplay_videos && mediaRef.current) {
+      if (
+        user.plus &&
+        !user.disable_autoplay_videos &&
+        mediaRef.current &&
+        !settingsStore.dataSaverEnabled
+      ) {
         mediaRef.current.playVideo(true);
       }
     } else {
--- src/newsfeed/activity/Activity.tsx
+++ src/newsfeed/activity/Activity.tsx
@@ -14,6 +14,7 @@ import {
 import * as entities from 'entities';
 
 import ExplicitText from '../../common/components/explicit/ExplicitText';
+import settingsStore from '../../settings/SettingsStore';
 import OwnerBlock from './OwnerBlock';
 import Actions from './Actions';
 import formatDate from '../../common/helpers/date';
@@ -158,7 +159,11 @@ export default class Activity extends Component<PropsType> {
         if (type === 'video') {
           if (visible) {
             const user = sessionService.getUser();
-            if (user.plus && !user.disable_autoplay_videos) {
+            if (
+              user.plus &&
+              !user.disable_autoplay_videos &&
+              !settingsStore.dataSaverEnabled
+            ) {
               const state = NavigationService.getCurrentState();
               // sound only for ActivityScreen (Full screen)
               const sound = state.name === 'Activity';
