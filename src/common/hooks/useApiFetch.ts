dd07cf078c95b21ab85c8c014628b03677f79b0e
Manishoo <maanishooshtari@gmail.com>
2020-09-27 13:21:52 +0330

    Add Referrals Console

--- src/common/hooks/useApiFetch.ts
+++ src/common/hooks/useApiFetch.ts
@@ -1,10 +1,10 @@
-import React from 'react';
-import { useLocalStore, useAsObservableSource } from 'mobx-react';
-import apiService from '../services/api.service';
 import { reaction } from 'mobx';
+import { useAsObservableSource, useLocalStore } from 'mobx-react';
+import React, { useEffect } from 'react';
+import apiService from '../services/api.service';
 
-const createStore = ({ url }) => ({
-  loading: true,
+const createStore = ({ url, method = 'get' }) => ({
+  loading: false,
   result: null,
   error: null,
   setResult(v: any) {
@@ -16,11 +16,11 @@ const createStore = ({ url }) => ({
   setError(e) {
     this.error = e;
   },
-  async fetch(params: object) {
+  async fetch(data: object = {}) {
     this.setLoading(true);
     this.setError(null);
     try {
-      const result = await apiService.get(url, params, this);
+      const result = await apiService[method](url, data);
       this.setResult(result);
     } catch (err) {
       console.log(err);
@@ -28,17 +28,42 @@ const createStore = ({ url }) => ({
     } finally {
       this.setLoading(false);
     }
+
+    return this.result;
+  },
+  async fetchMore(data = {}, updateState) {
+    this.setLoading(true);
+    this.setError(null);
+    try {
+      const result = await apiService.get(url, data);
+      this.setResult(updateState(result, this.result));
+    } catch (err) {
+      console.log(err);
+      this.setError(err);
+    } finally {
+      this.setLoading(false);
+    }
+
+    return this.result;
   },
 });
 
-export interface StateStore<T> {
+export interface FetchStore<T> {
   loading: boolean;
   result: T | null;
   error: any;
   setResult: (v: any) => void;
   setLoading: (v: boolean) => void;
   setError: (v: any) => void;
-  fetch: (object) => Promise<void>;
+  fetch: (object?) => Promise<any>;
+  fetchMore: (
+    data: any,
+    updateState: (newData: any, oldData: any) => any,
+  ) => Promise<any>;
+}
+
+export interface PostStore<T> extends FetchStore<T> {
+  post: (object?) => Promise<any>;
 }
 
 /**
@@ -49,19 +74,48 @@ export interface StateStore<T> {
  *
  * @param url string
  * @param params object
+ * @param reactToParams boolean
  */
 export default function useApiFetch<T>(
   url: string,
   params: object = {},
-): StateStore<T> {
-  const store: StateStore<T> = useLocalStore(createStore, { url });
+  reactToParams: boolean = true,
+): FetchStore<T> {
+  const store: FetchStore<T> = useLocalStore(createStore, { url });
   const observableParams = useAsObservableSource(params);
 
+  useEffect(() => {
+    store.fetch(params);
+  });
+
   React.useEffect(() => {
-    reaction(() => ({ ...observableParams }), store.fetch, {
-      fireImmediately: true,
-    });
-  }, [observableParams, store, url]);
+    if (reactToParams) {
+      reaction(() => ({ ...observableParams }), store.fetch, {
+        fireImmediately: true,
+      });
+    }
+  }, [reactToParams, observableParams, store, url]);
 
   return store;
+}
+
+/**
+ * The same hook as above but use to post data
+ *
+ * @param url string
+ * @param method string
+ */
+export function useApiPost<T>(
+  url: string,
+  method: string = 'post',
+): PostStore<T> {
+  const store: FetchStore<T> = useLocalStore(createStore, {
+    url,
+    method,
+  });
+
+  return {
+    ...store,
+    post: store.fetch,
+  };
 }
