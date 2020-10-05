29d06003ca8ec9d1b8c67166af012f115318feb8
Martin Santangelo <msantang78@gmail.com>
2020-10-05 16:20:01 -0300

    (feat) video thumb url without redirection and limit resolution

--- src/media/v2/mindsVideo/Video.tsx
+++ src/media/v2/mindsVideo/Video.tsx
@@ -6,6 +6,9 @@ import type CommentModel from '../../../comments/CommentModel';
 import { Video, ResizeMode, VideoReadyForDisplayEvent } from 'expo-av';
 import ThemedStyles from '../../../styles/ThemedStyles';
 import { toJS } from 'mobx';
+import mindsService from '../../../common/services/minds.service';
+import mediaProxyUrl from '../../../common/helpers/media-proxy-url';
+import { ImageURISource } from 'react-native';
 
 type PropsType = {
   entity?: ActivityModel | CommentModel;
@@ -13,6 +16,27 @@ type PropsType = {
   repeat?: boolean;
   resizeMode?: ResizeMode;
   onReadyForDisplay?: (event: VideoReadyForDisplayEvent) => void;
+};
+
+const getVideoThumb = (
+  entity?: ActivityModel | CommentModel,
+  size?: number,
+): ImageURISource | undefined => {
+  const source = entity
+    ? mindsService.settings.cinemr_url
+      ? {
+          uri: `${mindsService.settings.cinemr_url}${
+            entity.entity_guid || entity.guid
+          }/thumbnail-00001.png`,
+        }
+      : { uri: entity.get('custom_data.thumbnail_src') || entity.thumbnail_src }
+    : undefined;
+
+  if (source && size) {
+    source.uri = mediaProxyUrl(source.uri, size);
+  }
+
+  return source;
 };
 
 const ExpoVideo = observer(
@@ -26,9 +50,8 @@ const ExpoVideo = observer(
     const theme = ThemedStyles.style;
     const playbackObject = useRef<Video>(null);
 
-    const thumb_uri = entity
-      ? entity.get('custom_data.thumbnail_src') || entity.thumbnail_src
-      : null;
+    // limit the video thumb to a maximum size of 1024
+    const thumb_uri = useRef(getVideoThumb(entity, 1024)).current;
 
     const source = localStore.video.uri ? toJS(localStore.video) : undefined;
 
@@ -47,7 +70,7 @@ const ExpoVideo = observer(
         onError={localStore.onError}
         source={source}
         usePoster={!!thumb_uri}
-        posterSource={{ uri: thumb_uri }}
+        posterSource={thumb_uri}
         isLooping={repeat}
         resizeMode={resizeMode || 'contain'}
         useNativeControls={false}
