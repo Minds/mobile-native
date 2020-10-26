e4648372ebdb517018a4e645b48f40d15004c937
Martin Santangelo <msantang78@gmail.com>
2020-10-26 16:11:41 -0300

    (fix) composer reseting store when going back from options

--- src/compose/createComposeStore.js
+++ src/compose/createComposeStore.js
@@ -66,19 +66,13 @@ export default function ({ props, newsfeed }) {
     posting: false,
     group: null,
     postToPermaweb: false,
+    initialized: false,
     onScreenFocused() {
       const params = props.route.params;
-      if (
-        !params ||
-        (!params.entity &&
-          !params.mode &&
-          !params.media &&
-          !params.text &&
-          !params.noText &&
-          !params.portrait)
-      ) {
+      if (this.initialized || !params) {
         return;
       }
+      this.initialized = true;
 
       this.noText = Boolean(params.noText);
       this.portraitMode = params.portrait;
