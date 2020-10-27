2e91aeefcfbdd4b73756942bdb54a00dd58e5847
Martin Santangelo <msantang78@gmail.com>
2020-10-27 18:42:01 -0300

    (feat) bottom options component multiplatform

--- src/common/components/BottomButtonOptions.tsx
+++ src/common/components/BottomButtonOptions.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleProp, TextStyle } from 'react-native';
+import { Platform, StyleProp, StyleSheet, TextStyle } from 'react-native';
+import { View } from 'react-native-animatable';
 import { BottomSheet, ListItem } from 'react-native-elements';
 import ThemedStyles from '../../styles/ThemedStyles';
 
@@ -10,38 +11,63 @@ export type ItemType = {
 };
 
 export interface PropsType {
-  list: Array<ItemType>;
+  list: Array<Array<ItemType>>;
   isVisible: boolean;
 }
 
+const isIOS = Platform.OS === 'ios';
+/**
+ * Bottom options menu
+ */
 export default function BottomButtonOptions({ isVisible, list }: PropsType) {
   const theme = ThemedStyles.style;
+
   return (
     <BottomSheet
       isVisible={isVisible}
       modalProps={{ style: theme.borderRadius5 }}>
-      {list.map((l, i) => (
-        <ListItem
-          key={i}
-          containerStyle={[
-            theme.backgroundPrimary,
-            theme.borderPrimary,
-            i < list.length - 1 ? theme.borderBottomHair : null,
-          ]}
-          onPress={l.onPress}>
-          <ListItem.Content>
-            <ListItem.Title
-              style={[
-                theme.colorPrimaryText,
-                theme.paddingVertical,
-                theme.centered,
-                l.titleStyle,
-              ]}>
-              {l.title}
-            </ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-      ))}
+      <View style={theme.paddingBottom}>
+        {list.map((plist) =>
+          plist.map((l, i) => (
+            <ListItem
+              key={i}
+              containerStyle={[
+                theme.backgroundPrimary,
+                theme.marginHorizontal3x,
+                theme.borderPrimary,
+                i < plist.length - 1 ? theme.borderBottomHair : null,
+                i === 0 ? styles.borderRadiusTop : null,
+                i === plist.length - 1 ? styles.borderRadiusBottom : null,
+              ]}
+              onPress={l.onPress}>
+              <ListItem.Content>
+                <ListItem.Title
+                  style={[
+                    theme.colorPrimaryText,
+                    isIOS ? theme.paddingVertical : null,
+                    theme.fontXL,
+                    theme.centered,
+                    l.titleStyle,
+                  ]}>
+                  {l.title}
+                </ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+          )),
+        )}
+      </View>
     </BottomSheet>
   );
 }
+
+const styles = StyleSheet.create({
+  borderRadiusTop: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+  borderRadiusBottom: {
+    marginBottom: 10,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+});
