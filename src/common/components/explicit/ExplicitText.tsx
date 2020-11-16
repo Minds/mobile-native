92850066c2be0d9ffd07e4cafa9207fb601214e6
Martin Santangelo <msantang78@gmail.com>
2020-11-16 19:34:03 -0300

    (feat) deleted remind message

--- src/common/components/explicit/ExplicitText.tsx
+++ src/common/components/explicit/ExplicitText.tsx
@@ -74,6 +74,10 @@ export default class ExplicitText extends Component<PropsType, StateType> {
     const theme = ThemedStyles.style;
     const entity = this.props.entity;
 
+    if (this.props.entity.remind_deleted) {
+      return null;
+    }
+
     let title: string =
       entity.title && !entity.perma_url
         ? entities.decodeHTML(entity.title).trim()
