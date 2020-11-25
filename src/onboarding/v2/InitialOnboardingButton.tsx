a8be81249fb8bc230d8808625496237e7bf7c6d5
Martin Santangelo <msantang78@gmail.com>
2020-11-25 19:15:04 -0300

    (feat) replace tooltip by a animation

--- src/onboarding/v2/InitialOnboardingButton.tsx
+++ src/onboarding/v2/InitialOnboardingButton.tsx
@@ -1,19 +1,16 @@
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import * as Animatable from 'react-native-animatable';
 import React, { useEffect, useRef } from 'react';
 import moment from 'moment-timezone';
-import { View } from 'react-native';
 import MenuItem from '../../common/components/menus/MenuItem';
 import i18n from '../../common/services/i18n.service';
-import { LIGHT_THEME } from '../../styles/Colors';
 import ThemedStyles from '../../styles/ThemedStyles';
-import { Tooltip, Text } from 'react-native-elements';
-import { useDimensions } from '@react-native-community/hooks';
+import { Tooltip } from 'react-native-elements';
 import useOnboardingProgress from './useOnboardingProgress';
 import { observer } from 'mobx-react';
 import SettingsStore from '../../settings/SettingsStore';
 
 let shownOnce = false;
-
 /**
  * Initial onboarding button
  */
@@ -21,7 +18,7 @@ export default observer(function InitialOnboardingButton() {
   const theme = ThemedStyles.style;
   const tooltipRef = useRef<Tooltip>(null);
   const navigation = useNavigation();
-  const { width } = useDimensions().screen;
+  // const { width } = useDimensions().screen;
 
   // get onboarding progress
   const progressStore = useOnboardingProgress();
@@ -84,24 +81,14 @@ export default observer(function InitialOnboardingButton() {
   }
 
   return (
-    <View>
+    <Animatable.View
+      animation="pulse"
+      duration={700}
+      iterationDelay={100}
+      iterationCount={2}
+      useNativeDriver
+      delay={1500}>
       <MenuItem item={item} titleStyle={theme.bold} />
-      <Tooltip
-        ref={tooltipRef}
-        width={width * 0.7}
-        height={80}
-        withOverlay={false}
-        containerStyle={containerStyle}
-        popover={
-          <Text style={[theme.colorWhite, theme.fontL]}>
-            {i18n.t('onboarding.tooltipInitial1') + '\n'}
-            {i18n.t('onboarding.tooltipInitial2')}
-          </Text>
-        }
-        backgroundColor={LIGHT_THEME.link}
-      />
-    </View>
+    </Animatable.View>
   );
 });
-
-const containerStyle = { left: 20, borderRadius: 2 };
