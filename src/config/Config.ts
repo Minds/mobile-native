3e58a8b87995420cf3afe805b73f2a24f7552287
Elio <eliobricenov@gmail.com>
2020-12-08 23:43:22 -0400

    (wip): added blockchain token address

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -31,6 +31,10 @@ export const MINDS_ASSETS_CDN_URI = 'https://cdn-assets.minds.com/';
 // export const MINDS_CDN_URI = 'http://dev.minds.io/';
 
 export const BLOCKCHAIN_URI = 'https://www.minds.com/api/v2/blockchain/proxy/';
+
+export const BLOCKCHAIN_TOKEN_ADDRESS =
+  '0xb26631c6dda06ad89b93c71400d25692de89c068';
+
 // export const BLOCKCHAIN_URI = 'http://localhost:9545';
 export const MINDS_LINK_URI = 'https://www.minds.com/';
 export const CODE_PUSH_TOKEN = '';
