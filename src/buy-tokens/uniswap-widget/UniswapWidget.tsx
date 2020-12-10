3e1aac84838b0b06fff6502f4a3975657307ca6a
Elio <eliobricenov@gmail.com>
2020-12-09 23:38:51 -0400

    (feat) implemented webview modal component

--- src/buy-tokens/uniswap-widget/UniswapWidget.tsx
+++ src/buy-tokens/uniswap-widget/UniswapWidget.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import ThemedStyles from '../../styles/ThemedStyles';
-import Modal from 'react-native-modal';
-import Icon from 'react-native-vector-icons/AntDesign';
-import { WebView } from 'react-native-webview';
-import { StyleSheet, View } from 'react-native';
 import { BLOCKCHAIN_TOKEN_ADDRESS } from '../../config/Config';
+import WebviewModal from '../../common/components/WebviewModal';
 
 type UniswapAction = 'swap' | 'add';
 
 type Props = {
   isVisible: boolean;
   action?: UniswapAction;
-  onBackButtonPress: () => void;
   onCloseButtonPress: () => void;
 };
 
@@ -25,56 +20,16 @@ const getUniSwapURL = (action: UniswapAction) => {
   }
 };
 
-const modalInjectedScript =
-  'window.ReactNativeWebView.postMessage(Math.max(document.body.offsetHeight, document.body.scrollHeight));';
-
-const styles = StyleSheet.create({
-  closeButton: {
-    zIndex: 999,
-    position: 'absolute',
-    right: -10,
-    top: -10,
-    margin: 5,
-  },
-});
-
 export default function ({
   isVisible,
-  onBackButtonPress,
   onCloseButtonPress,
   action = 'swap',
 }: Props) {
-  const theme = ThemedStyles.style;
   return (
-    <Modal
+    <WebviewModal
       isVisible={isVisible}
-      coverScreen={false}
-      onBackButtonPress={onBackButtonPress}>
-      <View
-        style={[
-          theme.flexContainer,
-          theme.justifyCenter,
-          theme.borderRadius2x,
-        ]}>
-        <Icon
-          name="closecircleo"
-          onPress={onCloseButtonPress}
-          size={28}
-          style={[
-            theme.colorTertiaryText,
-            theme.positionAbsoluteTopRight,
-            styles.closeButton,
-          ]}
-        />
-        <WebView
-          source={{
-            uri: getUniSwapURL(action),
-          }}
-          bounces={true}
-          scrollEnabled={false}
-          injectedJavaScript={modalInjectedScript}
-        />
-      </View>
-    </Modal>
+      uri={getUniSwapURL(action)}
+      onCloseButtonPress={onCloseButtonPress}
+    />
   );
 }
