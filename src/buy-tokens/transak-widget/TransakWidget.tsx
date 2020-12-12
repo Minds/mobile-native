40df534b2a023f211c5b6a16190fcd56e7ecc1c1
Elio <eliobricenov@gmail.com>
2020-12-12 00:37:03 -0400

    (feat) added transak liquidity capaility

--- src/buy-tokens/transak-widget/TransakWidget.tsx
+++ src/buy-tokens/transak-widget/TransakWidget.tsx
@@ -1,71 +1,68 @@
 import React, { useEffect } from 'react';
 import Pusher from 'pusher-js/react-native';
-import jwt from 'jsonwebtoken';
-import {
-  BLOCKCHAIN_TOKEN_ADDRESS,
-  TRANSAK_API_KEY,
-  TRANSAK_PARTNER_API_SECRET,
-} from '../../config/Config';
 import WebviewModal from '../../common/components/WebviewModal';
+import { TRANSAK_API_KEY } from '../../config/Config';
 
-//TODO: define this configuration file
-const getBaseURL = (isProdEnvironment: boolean) =>
+const pusher = new Pusher('1d9ffac87de599c61283', { cluster: 'ap2' }); // Transak Public API;
+const partnerOrderId = '23487492'; // Can be any number. It is required for the iframe to work properly
+const channelName = `${TRANSAK_API_KEY}_${partnerOrderId}`;
+
+// TODO: Replace this based upon environment config
+const getTransakDomain = (isProdEnvironment: boolean) =>
   isProdEnvironment
     ? 'https://global.transak.com'
     : 'https://staging-global.transak.com';
 
-const transakURL = `${getBaseURL(
+const transakURL = `${getTransakDomain(
   false,
-)}?apiKey=${TRANSAK_API_KEY}&defaultCryptoCurrency=ETH&walletAddress=${BLOCKCHAIN_TOKEN_ADDRESS}`;
+)}?apiKey=${TRANSAK_API_KEY}&partnerOrderId=${partnerOrderId}&defaultCryptoCurrency=ETH`;
 
-const pusher = new Pusher('1d9ffac87de599c61283', { cluster: 'ap2' });
-
-const decrypt = function (encryptedOrderData: string | object) {
-  if (encryptedOrderData && typeof encryptedOrderData === 'string') {
-    try {
-      const decryptedOrderData = jwt.verify(
-        encryptedOrderData,
-        TRANSAK_PARTNER_API_SECRET,
-      );
-      if (decryptedOrderData && (decryptedOrderData as any).id) {
-        console.log({ orderData: decryptedOrderData });
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  }
-};
+export interface TransakOrderProcessed {
+  id: string;
+  status: string;
+  totalFeeInFiat: string;
+  walletAddress: string;
+  walletLink: string;
+  cryptoAmount: number;
+  cryptoCurrency: string;
+  fiatAmount: number;
+  fiatCurrency: string;
+  paymentOptionId: string;
+}
 
 type Props = {
   isVisible: boolean;
-  onOrderSuccessFull: (response) => void;
+  onOrderProcessed: (response: TransakOrderProcessed) => void;
   onError: (response) => void;
   onCloseButtonPress: () => void;
 };
 
 export default function ({
   isVisible,
-  onOrderSuccessFull,
+  onOrderProcessed,
   onCloseButtonPress,
+  onError,
 }: Props) {
   useEffect(() => {
-    pusher.subscribe(TRANSAK_API_KEY);
+    const channel = pusher.subscribe(channelName);
 
-    //receive updates of all the orders
-    pusher.bind_global((orderId, encryptedOrderData) => {
-      console.log(`Order update ${orderId}`);
-      onOrderSuccessFull(decrypt(encryptedOrderData));
+    channel.bind('ORDER_PROCESSING', (orderData: TransakOrderProcessed) => {
+      onOrderProcessed(orderData);
+    });
+
+    channel.bind('ORDER_FAILED', (orderData) => {
+      onError(orderData);
     });
 
     return () => {
-      pusher.unsubscribe(TRANSAK_API_KEY);
+      pusher.unsubscribe(channelName);
     };
-  }, [onOrderSuccessFull]);
+  }, [onOrderProcessed, onError]);
+
   return (
     <WebviewModal
       isVisible={isVisible}
       uri={transakURL}
-      closeButtonPosition={'left'}
       onCloseButtonPress={onCloseButtonPress}
     />
   );
