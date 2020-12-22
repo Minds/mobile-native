918d9cbedad2dc028dbce8075220e2b77b1d07c4
Martin Santangelo <msantang78@gmail.com>
2020-12-22 11:25:02 -0300

    (fix) test process ci

--- __tests__/App.js
+++ __tests__/App.js
@@ -4,19 +4,18 @@ import App from '../App';
 import sqliteStorageProviderService from '../src/common/services/sqlite-storage-provider.service';
 import logService from '../src/common/services/log.service';
 import MindsVideoV2 from '../src/media/v2/mindsVideo/MindsVideo';
-import {
-  BackHandler,
-} from 'react-native';
+import { BackHandler } from 'react-native';
 import ShareMenu from 'react-native-share-menu';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
-
+jest.mock('react-native-reanimated', () =>
+  require('react-native-reanimated/mock'),
+);
 
 // mock backhandler
 BackHandler.addEventListener = jest.fn();
-jest.mock('../src/common/services/sqlite-storage-provider.service')
+jest.mock('../src/common/services/sqlite-storage-provider.service');
 jest.mock('../src/common/services/log.service', () => {});
 jest.mock('../src/common/services/push.service');
 
@@ -46,8 +45,6 @@ jest.mock('../src/common/services/translation.service');
 jest.mock('../src/common/helpers/abortableFetch');
 jest.mock('../src/tos/TosModal', () => 'TosModal');
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
-  );
+it('renders correctly', async () => {
+  const tree = renderer.create(<App />);
 });
