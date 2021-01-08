667cc536e67de2aba7c4c0068035dbff4d1dfac0
Juan Manuel Solaro <juanmsolaro@gmail.com>
2021-01-08 08:24:13 -0300

    new buttons design

--- src/channel/v2/buttons/Join.tsx
+++ src/channel/v2/buttons/Join.tsx
@@ -1,21 +1,14 @@
 import React, { useCallback } from 'react';
-import { Platform } from 'react-native';
 import Button from '../../../common/components/Button';
 import i18n from '../../../common/services/i18n.service';
-import ThemedStyles from '../../../styles/ThemedStyles';
 import { ChannelButtonsPropsType } from '../ChannelButtons';
-import { styles } from './styles';
 
 type PropsType = {
   showSubscribe: boolean | undefined;
   navigation: any;
 } & ChannelButtonsPropsType;
 
-const isIos = Platform.OS === 'ios';
-
-const Join = ({ showSubscribe, navigation, ...props }: PropsType) => {
-  const theme = ThemedStyles.style;
-
+const Join = ({ navigation, ...props }: PropsType) => {
   const join = useCallback(() => {
     if (props.store.channel) {
       navigation.push('JoinMembershipScreen', {
@@ -25,24 +18,7 @@ const Join = ({ showSubscribe, navigation, ...props }: PropsType) => {
     }
   }, [navigation, props.store.channel, props.store.tiers]);
 
-  return (
-    <Button
-      color={
-        showSubscribe
-          ? ThemedStyles.getColor('secondary_background')
-          : ThemedStyles.getColor('green')
-      }
-      text={i18n.t('join')}
-      textStyle={[
-        isIos ? theme.fontL : theme.fontM,
-        !ThemedStyles.theme && !showSubscribe ? null : theme.colorPrimaryText,
-      ]}
-      containerStyle={styles.button}
-      textColor="white"
-      onPress={join}
-      inverted
-    />
-  );
+  return <Button text={i18n.t('join')} onPress={join} xSmall />;
 };
 
 export default Join;
