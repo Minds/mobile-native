803fbc17486e1b93c44f3ff4c65edc850b2cc0d5
Martin Santangelo <msantang78@gmail.com>
2021-01-20 17:39:50 -0300

    (chore) remove unused import

--- src/comments/v2/CommentListHeader.tsx
+++ src/comments/v2/CommentListHeader.tsx
@@ -1,7 +1,7 @@
 import { TouchableOpacity } from '@gorhom/bottom-sheet';
 import Icon from 'react-native-vector-icons/Feather';
 import { observer } from 'mobx-react';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import sessionService from '../../common/services/session.service';
