730754d1fd4db6d552e93b2f36293033030fa338
Juan Manuel Solaro <juanmsolaro@gmail.com>
2021-01-22 16:21:19 -0300

    AccordionSet component

---
+++ src/common/components/AccordionSet.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useLocalStore } from 'mobx-react';
+import Accordion from 'react-native-collapsible/Accordion';
+
+export type AccordionDataType = {
+  title: string;
+  children: React.ReactElement<{}>;
+  subtitle?: string;
+  tooltip?: string;
+};
+
+export type RenderFunction = (
+  content: any,
+  index: number,
+  isActive: boolean,
+  sections: Array<AccordionDataType>,
+) => React.ReactElement<{}>;
+
+type PropsType = {
+  data: Array<AccordionDataType>;
+  headerComponent: RenderFunction;
+  contentComponent: RenderFunction;
+};
+
+const AccordionSet = (props: PropsType) => {
+  const localStore = useLocalStore(() => ({
+    activeSections: [] as Array<number>,
+    setActiveSections(index: number) {
+      this.activeSections = [index];
+    },
+    onChange(indexes: number[]) {
+      this.setActiveSections(indexes[0]);
+    },
+  }));
+  return (
+    <Accordion
+      sections={props.data}
+      activeSections={localStore.activeSections}
+      renderHeader={props.headerComponent}
+      renderContent={props.contentComponent}
+      onChange={localStore.onChange}
+    />
+  );
+};
+
+export default AccordionSet;
--- src/wallet/v3/currency-tabs/UsdEarnings.tsx
+++ src/wallet/v3/currency-tabs/UsdEarnings.tsx
@@ -7,7 +7,12 @@ import { View, Text, StyleSheet } from 'react-native';
 import { WalletStoreType } from '../../v2/createWalletStore';
 import ThemedStyles from '../../../styles/ThemedStyles';
 import { ScrollView } from 'react-native-gesture-handler';
+import AccordionSet, {
+  AccordionDataType,
+  RenderFunction,
+} from '../../../common/components/AccordionSet';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { observer } from 'mobx-react';
 
 type PropsType = {
   navigation: WalletScreenNavigationProp;
@@ -15,15 +20,61 @@ type PropsType = {
   route: WalletScreenRouteProp;
 };
 
+const HeaderComponent: RenderFunction = (
+  content: AccordionDataType,
+  index: number,
+  isActive: boolean,
+) => {
+  const theme = ThemedStyles.style;
+  return (
+    <View style={theme.rowJustifySpaceBetween}>
+      <Text>{content.title}</Text>
+      <Text>{content.subtitle}</Text>
+      <Icon
+        name={`chevron-${isActive ? 'up' : 'down'}`}
+        size={14}
+        color={ThemedStyles.getColor('secondary_text')}
+      />
+    </View>
+  );
+};
+
+const ContentComponent: RenderFunction = (content: AccordionDataType) =>
+  content.children;
+
 const UsdEarnings = observer(({ walletStore, navigation }: PropsType) => {
   const theme = ThemedStyles.style;
 
+  const accordionData: Array<AccordionDataType> = [
+    {
+      title: 'Minds Pro',
+      subtitle: '$100',
+      children: (
+        <View>
+          <Text>minds pro</Text>
+        </View>
+      ),
+    },
+    {
+      title: 'Minds+',
+      subtitle: '$0.35',
+      children: (
+        <View>
+          <Text>minds plus</Text>
+        </View>
+      ),
+    },
+  ];
+
   return (
     <ScrollView
-      contentContainerStyle={[
-        theme.paddingTop4x,
-        theme.paddingHorizontal3x,
-      ]}></ScrollView>
+      contentContainerStyle={[theme.paddingTop4x, theme.paddingHorizontal3x]}>
+      <AccordionSet
+        data={accordionData}
+        headerComponent={HeaderComponent}
+        contentComponent={ContentComponent}
+      />
+    </ScrollView>
   );
 });
 
