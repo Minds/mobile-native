0680e30eeb0fda52d60969bb300d2e716a1b2e28
Martin Santangelo <msantang78@gmail.com>
2021-02-02 14:53:34 -0300

    test session storage

--- src/connection-test/ConnectionTestScreen.tsx
+++ src/connection-test/ConnectionTestScreen.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { Linking, ScrollView, Text, View } from 'react-native';
 import WalletConnectProvider from '@walletconnect/web3-provider';
+
 import Web3 from 'web3';
 import Button from '../common/components/Button';
 import ThemedStyles from '../styles/ThemedStyles';
 import { observer, useLocalStore } from 'mobx-react';
-import { getConnector } from '../blockchain/services/WalletConnectService';
+import SessionStorage from '../blockchain/walletconnect/SessionStorage';
+import WalletConnect from '../blockchain/walletconnect/WalletConnect';
+
+const MINDS_METADATA = {
+  description: 'Minds',
+  url: 'https://www.minds.com',
+  icons: ['https://cdn.minds.com/icon/100000000000000519/large/1589923649'],
+  name: 'Minds',
+};
 
 type Store = {
   web3: Web3 | null;
@@ -25,22 +34,21 @@ type Store = {
   sendTestTransaction: () => Promise<void>;
   signTestMessage: () => Promise<void>;
   resetConnection: () => void;
+  connect: () => void;
+  init: () => void;
 };
 
 export default observer(() => {
   const theme = ThemedStyles.style;
   const store = useLocalStore<Store>(createStore);
 
+  React.useEffect(() => {
+    store.init();
+  }, []);
+
   const startConnection = async () => {
     try {
-      const provider = await getConnector();
-      const web3 = new Web3(provider as any);
-      store.setWeb3(web3);
-      store.setProvider(provider);
-      store.setConnected(true);
-      store.setAccounts(provider.accounts);
-      store.setAddress(provider.accounts[0]);
-      store.setChainId(provider.chainId);
+      store.connect();
     } catch (error) {
       console.log('error =>', error);
     }
@@ -78,18 +86,25 @@ export default observer(() => {
           )}
         </>
       ) : (
-        <Button
-          onPress={startConnection}
-          text="Connect"
-          containerStyle={[
-            theme.transparentButton,
-            theme.paddingVertical3x,
-            theme.fullWidth,
-            theme.marginTop,
-            theme.borderPrimary,
-          ]}
-          textStyle={theme.buttonText}
-        />
+        <>
+          <Button
+            onPress={startConnection}
+            text="Connect"
+            containerStyle={[
+              theme.transparentButton,
+              theme.paddingVertical3x,
+              theme.fullWidth,
+              theme.marginTop,
+              theme.borderPrimary,
+            ]}
+            textStyle={theme.buttonText}
+          />
+          <Button
+            containerStyle={[theme.marginBottom3x]}
+            onPress={store.signTestMessage}
+            text="Sign Message"
+          />
+        </>
       )}
     </ScrollView>
   );
@@ -103,6 +118,57 @@ const createStore = (): Store => ({
   accounts: null,
   address: null,
   formattedResult: null,
+  async init() {
+    const sessionStorage = new SessionStorage();
+    await sessionStorage.loadFromStorage();
+    const provider = new WalletConnectProvider({
+      infuraId: '612f7850f87540d4a0c41796284ef45f',
+      connector: new WalletConnect(
+        {
+          bridge: 'https://bridge.walletconnect.org',
+          clientMeta: MINDS_METADATA,
+        },
+        undefined,
+        // sessionStorage,
+      ),
+      chainId: 4,
+    });
+    // if (sessionStorage.session) {
+    //   provider.enable();
+    //   this.setConnected(true);
+    // }
+    console.log(provider);
+    const web3 = new Web3(provider as any);
+    this.setWeb3(web3);
+    this.setProvider(provider);
+
+    // open wallet using deep linking
+    this.provider?.connector.on('display_uri', (_, payload) => {
+      Linking.openURL(payload.params[0]);
+    });
+
+    this.provider?.on('connect', async () => {
+      this.setConnected(true);
+      this.setAccounts(provider.accounts);
+      this.setAddress(provider.accounts[0]);
+      this.setChainId(provider.chainId);
+    });
+    this.provider?.on('disconnect', async () => {
+      this.setConnected(false);
+      this.setAccounts(null);
+      this.setAddress(null);
+      this.setChainId(null);
+    });
+  },
+  connect() {
+    if (!this.provider) {
+      throw Error('Call init first');
+    }
+    if (this.connected) {
+      return;
+    }
+    this.provider.enable();
+  },
   setWeb3(newValue: Store['web3']) {
     this.web3 = newValue;
   },
@@ -158,29 +224,31 @@ const createStore = (): Store => ({
     }
 
     try {
-      const message = 'Hello World';
-      const result = await this.web3.eth.sign(message, this.address);
+      const balance = await this.web3.eth.getBalance(this.address);
+      alert(this.web3.utils.fromWei(balance, 'ether'));
+      // const message = 'Hello World';
+      // const result = await this.web3.eth.sign(message, this.address);
 
-      console.log('formatted', {
-        method: 'personal_sign',
-        address: this.address,
-        valid: true,
-        result,
-      });
+      // console.log('formatted', {
+      //   method: 'personal_sign',
+      //   address: this.address,
+      //   valid: true,
+      //   result,
+      // });
 
-      this.setFormattedResult({
-        method: 'personal_sign',
-        address: this.address,
-        valid: true,
-        result,
-      });
+      // this.setFormattedResult({
+      //   method: 'personal_sign',
+      //   address: this.address,
+      //   valid: true,
+      //   result,
+      // });
     } catch (error) {
       console.log('error =>', error);
     }
   },
   resetConnection() {
+    this.provider?.disconnect();
     this.setConnected(false);
-    this.setProvider(null);
     this.setChainId(null);
     this.setAccounts(null);
     this.setAddress(null);
