d4640253a7bf179e0cd987aa6ec215e5c44ebec7
Martin Santangelo <msantang78@gmail.com>
2021-03-31 09:47:53 -0300

    (chore) remove canary

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -5,7 +5,7 @@ import DeviceInfo from 'react-native-device-info';
 
 // Send staging cookie to api
 export const MINDS_STAGING = false;
-export const MINDS_CANARY = true;
+export const MINDS_CANARY = false;
 
 // network timeout time
 export const NETWORK_TIMEOUT = 15000;
