6c49866e6f6acb95da72cf73506ea35de75a365a
Martin Santangelo <msantang78@gmail.com>
2021-03-31 15:48:55 -0300

    (feat) build release json in the CI

--- tasks/generate_release_json.js
+++ tasks/generate_release_json.js
@@ -6,7 +6,13 @@ const releases = require('../releases.json');
 const hash256 = crypto.createHash('sha256');
 const hash512 = crypto.createHash('sha512');
 const hashmd5 = crypto.createHash('md5');
+const { parser } = require('keep-a-changelog');
+
+// open APK
 const input = fs.createReadStream(filepath);
+
+//Parse a changelog file
+const changelogFile = parser(fs.readFileSync('./CHANGELOG.md', 'UTF-8'));
 
 // read version
 const gradleProperties = fs.readFileSync('./android/gradle.properties');
@@ -14,7 +20,7 @@ const gradleProperties = fs.readFileSync('./android/gradle.properties');
 let version = gradleProperties.toString().match(/versionName=(.*)/);
 if (version) {
   version = version[1];
-  releases.versions.forEach(v => {
+  releases.versions.forEach((v) => {
     if (v.version === version) {
       console.log('The version already exist to the json file');
       process.exit(1);
@@ -24,6 +30,12 @@ if (version) {
   console.log('Error reading the version');
   process.exit(1);
 }
+
+// check changelog
+const changelog =
+  changelogFile.releases[0].version.raw.trim() == version.trim()
+    ? changelogFile.releases[0].changes.get('changed').map((c) => c.title)
+    : [];
 
 const filename = path.basename(filepath);
 
@@ -35,11 +47,11 @@ input.on('readable', () => {
     hashmd5.update(data);
   } else {
     const v = {
-      version: version,
-      timestamp: (new Date()).timestamp,
-      href: 'https://cdn-assets.minds.com/android/' + filename,
+      version,
+      timestamp: new Date().timestamp,
+      href: 'https://cdn-assets.minds.com/mobile/' + filename,
       sourceHref: 'https://gitlab.com/minds/mobile-native/commits/v' + version,
-      changelog: ['Changelog here'],
+      changelog,
       unstable: false,
       hashes: [
         {
@@ -58,6 +70,13 @@ input.on('readable', () => {
     };
     releases.versions.unshift(v);
 
+    // Write releases json
     fs.writeFileSync('./releases.json', JSON.stringify(releases, null, 2));
+
+    // Write changelog for fastlane
+    fs.writeFileSync(
+      `./android/fastlane/metadata/android/en-US/changelogs/${version.trim()}.txt`,
+      changelog.join('\n'),
+    );
   }
 });
