1dbb3b14f4d56a14c6c5b2442d6a195096b9b9a5
Martin Santangelo <msantang78@gmail.com>
2021-04-15 10:16:04 -0300

    (fix) animated gif upload on iOS

--- src/common/stores/AttachmentStore.ts
+++ src/common/stores/AttachmentStore.ts
@@ -77,12 +77,11 @@ export default class AttachmentStore {
       }
 
       // fix camera roll gif issue
-      if (media.type === 'image' && media.fileName) {
-        const extension = media.fileName.split('.').pop();
+      if (media.type === 'image/jpeg' && media.filename) {
+        const extension = media.filename.split('.').pop();
         if (extension && extension.toLowerCase() === 'gif') {
           media.type = 'image/gif';
-          const appleId = media.uri.substring(5, 41);
-          media.uri = `assets-library://asset/asset.GIF?id=${appleId}&ext=GIF`;
+          media.uri = `assets-library://asset/asset.GIF?id=${media.localIdentifier}&ext=GIF`;
         }
       }
     }
