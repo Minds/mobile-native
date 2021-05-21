347eaf17e336ff649ab9b88f4ccf008e4bc014e3
Martin Santangelo <msantang78@gmail.com>
2021-05-21 15:15:42 -0300

    (fix) video thumb fix

--- src/common/helpers/get-video-thumbnail.ts
+++ src/common/helpers/get-video-thumbnail.ts
@@ -1,22 +1,13 @@
 import { Source } from 'react-native-fast-image';
 import CommentModel from '../../comments/v2/CommentModel';
 import ActivityModel from '../../newsfeed/ActivityModel';
-import mindsService from '../services/minds.service';
 import mediaProxyUrl from './media-proxy-url';
 
 const getVideoThumb = (
-  entity?: ActivityModel | CommentModel,
+  entity: ActivityModel | CommentModel,
   size?: number,
 ): Pick<Source, 'uri'> | undefined => {
-  const source = entity
-    ? mindsService.settings && mindsService.settings.cinemr_url
-      ? {
-          uri: `${mindsService.settings.cinemr_url}${
-            entity.entity_guid || entity.guid
-          }/thumbnail-00001.png`,
-        }
-      : { uri: entity.get('custom_data.thumbnail_src') || entity.thumbnail_src }
-    : undefined;
+  const source = entity.getThumbSource('xlarge');
 
   if (source && size) {
     source.uri = mediaProxyUrl(source.uri, size);
