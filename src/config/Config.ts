9af44ce682259526d2a22034efe51d163c64c42b
Juan Manuel Solaro <juanmsolaro@gmail.com>
2021-05-26 13:21:21 -0300

    remove staging cookie

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -4,8 +4,8 @@ import { Platform } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 
 // Send staging cookie to api
-export const MINDS_STAGING = true;
-export const MINDS_CANARY = true;
+export const MINDS_STAGING = false;
+export const MINDS_CANARY = false;
 
 // network timeout time
 export const NETWORK_TIMEOUT = 15000;
