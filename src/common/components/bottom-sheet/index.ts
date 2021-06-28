bdf3662504b5ebcaebe21d14ae0cf04a9584618f
Juan Manuel Solaro <juanmsolaro@gmail.com>
2021-06-28 11:01:06 -0300

    replace transaction filter and wallet menu with bottomsheet

---
+++ src/common/components/bottom-sheet/index.ts
@@ -0,0 +1,7 @@
+import BottomSheet from './BottomSheet';
+import BottomSheetButton from './BottomSheetButton';
+import MenuItem from './MenuItem';
+import RadioButton from './RadioButton';
+import SectionTitle from './SectionTitle';
+
+export { BottomSheet, MenuItem, RadioButton, SectionTitle, BottomSheetButton };
--- src/wallet/v3/transaction-list/components/Header.tsx
+++ src/wallet/v3/transaction-list/components/Header.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import MIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-
+import { View, Text } from 'react-native';
+import Filter from './Filter';
 import ThemedStyles from '../../../../styles/ThemedStyles';
 import i18n from '../../../../common/services/i18n.service';
-import { AvatarIcon } from './Icons';
+import { TokensTransactionsListStoreType } from '../../../v2/TransactionList/createTokensTransactionsStore';
+import { AvatarIcon } from '../../../v2/TransactionList/components/Icons';
+import { CashTransactionsListStoreType } from '../../../v2/TransactionList/createCashTransactionsStore';
 
 type PropsType = {
-  showFilter: () => void;
+  store: TokensTransactionsListStoreType | CashTransactionsListStoreType;
 };
 
-const Header = ({ showFilter }: PropsType) => {
+const Header = ({ store }: PropsType) => {
   const theme = ThemedStyles.style;
   const alignedCenterRow = [theme.rowJustifyStart, theme.alignCenter];
+
+  const renderFilter = store.setTransactionType !== undefined;
 
   return (
     <View style={theme.marginBottom3x}>
@@ -20,20 +23,11 @@ const Header = ({ showFilter }: PropsType) => {
         <Text style={theme.colorSecondaryText}>
           {i18n.t('wallet.transactions.pending')}
         </Text>
-        {/* <TouchableOpacity onPress={() => true}> */}
-        <View style={alignedCenterRow}>
-          <TouchableOpacity onPress={showFilter}>
-            <MIcon
-              name={'filter-variant'}
-              color={ThemedStyles.getColor('Icon')}
-              size={28}
-            />
-            <Text style={theme.colorSecondaryText}>
-              {i18n.t('wallet.transactions.filter')}
-            </Text>
-          </TouchableOpacity>
-        </View>
-        {/* </TouchableOpacity> */}
+        {renderFilter && (
+          <View style={alignedCenterRow}>
+            <Filter store={store} />
+          </View>
+        )}
       </View>
       <View style={alignedCenterRow}>
         <AvatarIcon name="trending-up" />
