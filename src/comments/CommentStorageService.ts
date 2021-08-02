6001c67edaecee1c28f281d364587b85d1d51ef5
Martin Santangelo <msantang78@gmail.com>
2021-08-02 15:14:07 -0300

    (refactor) comments offline cache to use mmkv

--- src/comments/CommentStorageService.ts
+++ src/comments/CommentStorageService.ts
@@ -1,27 +1,10 @@
-//@ts-nocheck
-import sqliteStorageProviderService from '../common/services/sqlite-storage-provider.service';
 import logService from '../common/services/log.service';
-import moment from 'moment';
+import { storages } from '../common/services/storage/storages.service';
 
 /**
  * Comment storage service
  */
 export class CommentStorageService {
-  /**
-   * @var {SqliteService}
-   */
-  db;
-
-  /**
-   * Get the sqlite service
-   */
-  async getDb() {
-    if (!this.db) {
-      this.db = await sqliteStorageProviderService.get();
-    }
-    return this.db;
-  }
-
   /**
    * Read a page from the storage
    *
@@ -31,7 +14,7 @@ export class CommentStorageService {
    * @param {string} offset
    * @param {string} focusedUrn
    */
-  async read(
+  read(
     entityGuid: string,
     parentPath: string,
     descending: boolean,
@@ -39,26 +22,10 @@ export class CommentStorageService {
     focusedUrn: string,
   ) {
     try {
-      await this.getDb();
-
-      const params = [
-        entityGuid,
-        parentPath,
-        descending,
-        offset || '',
-        focusedUrn || '',
-      ];
-
-      const [result] = await this.db.executeSql(
-        'SELECT data FROM comments_feeds WHERE parent = ? AND parent_path = ? AND descending = ? AND offset = ? AND focused_urn = ?;',
-        params,
-      );
-
-      const rows = result.rows.raw();
-
-      if (!rows[0] || !rows[0].data) return null;
-
-      return JSON.parse(rows[0].data);
+      const key = `comm:${entityGuid}:${parentPath}:${offset}:${
+        focusedUrn || ''
+      }:${descending ? '0' : '1'}`;
+      return storages.userCache?.getMap(key);
     } catch (err) {
       logService.exception('[CommentStorageService]', err);
       return null;
@@ -75,7 +42,7 @@ export class CommentStorageService {
    * @param {string} focusedUrn
    * @param {Object} data
    */
-  async write(
+  write(
     entityGuid: string,
     parentPath: string,
     descending: boolean,
@@ -84,19 +51,10 @@ export class CommentStorageService {
     data: Object,
   ) {
     try {
-      await this.getDb();
-      await this.db.executeSql(
-        'REPLACE INTO comments_feeds (parent, parent_path, descending, offset, focused_urn, data, updated) values (?,?,?,?,?,?,?)',
-        [
-          entityGuid,
-          parentPath,
-          descending,
-          offset || '',
-          focusedUrn || '',
-          JSON.stringify(this.clean(data)),
-          Math.floor(Date.now() / 1000),
-        ],
-      );
+      const key = `comments:${entityGuid}:${parentPath}:${offset}:${
+        focusedUrn || ''
+      }:${descending ? '0' : '1'}`;
+      return storages.userCache?.setMap(key, this.clean(data));
     } catch (err) {
       logService.exception('[CommentStorageService]', err);
       console.log(err);
@@ -104,32 +62,11 @@ export class CommentStorageService {
   }
 
   /**
-   * Remove all comments feeds
-   */
-  async removeAll() {
-    await this.getDb();
-    return this.db.executeSql('DELETE FROM comments_feeds');
-  }
-
-  /**
-   * Remove comments feeds older than given days
-   *
-   * @param {integer} days
-   */
-  async removeOlderThan(days) {
-    const when = moment().subtract(days, 'days');
-    await this.getDb();
-    this.db.executeSql('DELETE FROM comments_feeds WHERE updated < ?', [
-      when.format('X'),
-    ]);
-  }
-
-  /**
    * Clean repeated data to reduce the stored size
    *
    * @param {Object} data
    */
-  clean(data: Object) {
+  clean(data: { comments: Array<any>; status?: string }) {
     data.comments.forEach(comment => {
       delete comment.luid;
       delete comment.body;
