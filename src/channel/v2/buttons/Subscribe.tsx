2cf6ab361134a1efb63ce1865114d562bbc81ced
Mani Shooshtari <mani@shooshtari.com>
2021-08-12 14:25:49 +0300

    (feat) make subscribe button more clickable

--- src/channel/v2/buttons/Subscribe.tsx
+++ src/channel/v2/buttons/Subscribe.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Button from '../../../common/components/Button';
 import i18n from '../../../common/services/i18n.service';
+import ThemedStyles, { useStyle } from '../../../styles/ThemedStyles';
 import { ChannelButtonsPropsType } from '../ChannelButtons';
 
 const Subscribe = (props: ChannelButtonsPropsType) => {
-  const subscriptionText = '+ ' + i18n.t('channel.subscribe');
+  const subscriptionText = i18n.t('channel.subscribe');
+  const buttonStyle = useStyle({
+    borderWidth: 2,
+    borderColor: ThemedStyles.getColor('Link'),
+    padding: 8,
+    paddingHorizontal: 10,
+    backgroundColor: ThemedStyles.getColor('Link'),
+  });
+
   return (
     <Button
       text={subscriptionText}
       onPress={props.store.channel!.toggleSubscription}
       xSmall
+      color={ThemedStyles.getColor('White')}
+      style={buttonStyle}
     />
   );
 };
