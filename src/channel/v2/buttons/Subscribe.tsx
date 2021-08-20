5f26185fc9f89c3353f4df96e9dc0024562a3479
Mani Shooshtari <mani@shooshtari.com>
2021-08-20 18:39:55 +0300

    (feat) ask before unsubscribing

--- src/channel/v2/buttons/Subscribe.tsx
+++ src/channel/v2/buttons/Subscribe.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Button from '../../../common/components/Button';
 import i18n from '../../../common/services/i18n.service';
 import UserModel from '../../UserModel';
 import { observer } from 'mobx-react';
+import { Alert } from 'react-native';
 
 const Subscribe = (props: { channel: UserModel; testID?: string }) => {
   const subscriptionText = props.channel.subscribed
     ? i18n.t('channel.subscribed')
     : i18n.t('channel.subscribe');
 
+  const onSubscriptionPress = useCallback(() => {
+    if (props.channel.subscribed) {
+      Alert.alert(i18n.t('attention'), i18n.t('channel.confirmUnsubscribe'), [
+        {
+          text: i18n.t('yesImSure'),
+          onPress: () => props.channel.toggleSubscription(),
+        },
+        { text: i18n.t('no') },
+      ]);
+    } else {
+      return props.channel.toggleSubscription();
+    }
+  }, [props.channel.subscribed, props.channel.toggleSubscription]);
+
   return (
     <Button
       action={!props.channel.subscribed}
       activeOpacity={0.7}
       text={subscriptionText}
-      onPress={props.channel!.toggleSubscription}
+      onPress={onSubscriptionPress}
       xSmall
       testID={props.testID}
     />
