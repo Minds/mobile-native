c10e8b71a1d78f2f75ceba4b14939a0990431f6a
Martin Santangelo <msantang78@gmail.com>
2021-08-26 10:50:47 -0300

    (fix) FadeFromBottom typing

--- src/common/components/animations/FadeFromBottom.tsx
+++ src/common/components/animations/FadeFromBottom.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
-import { View as MotiView } from 'moti';
+import { MotiProps, View as MotiView } from 'moti';
+import { ViewProps, ViewStyle } from 'react-native';
 
 const from = { opacity: 0, translateY: 50 };
 const animate = { opacity: 1, translateY: 0 };
@@ -7,7 +8,10 @@ const animate = { opacity: 1, translateY: 0 };
 /**
  * Fade content from bottom
  */
-export const FadeFromBottom: FC = ({ children, ...props }) => {
+export const FadeFromBottom: FC<ViewProps & MotiProps<ViewStyle>> = ({
+  children,
+  ...props
+}) => {
   return (
     <MotiView {...props} from={from} animate={animate}>
       {children}
