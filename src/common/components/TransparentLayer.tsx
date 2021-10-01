d22c9c399b2908f55fe72e35c46991e1fbd8d593
Martin Santangelo <msantang78@gmail.com>
2021-10-01 16:31:48 -0300

    (chore) fix transparent layer

--- src/common/components/TransparentLayer.tsx
+++ src/common/components/TransparentLayer.tsx
@@ -2,20 +2,12 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
-const colors = ['rgba(0,0,0,0.00)', 'rgba(0,0,0,0.20)', 'rgba(0,0,0,0.5)'];
+const colors = ['rgba(0,0,0,0.80)', 'rgba(0,0,0,0.2)'];
 
 export default function TransparentLayer() {
   return (
     <View style={StyleSheet.absoluteFill}>
-      <View style={styles.layer} />
       <LinearGradient style={StyleSheet.absoluteFill} colors={colors} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  layer: {
-    backgroundColor: 'rgba(37, 46, 49, 0.6)',
-    flex: 1,
-  },
-});
