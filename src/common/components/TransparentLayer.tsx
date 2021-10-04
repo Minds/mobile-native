c2542e438c68b7e3881573deb7cbd5929b1f91b5
Martin Santangelo <msantang78@gmail.com>
2021-10-04 15:24:00 -0300

    (fix) transparent layers

--- src/common/components/TransparentLayer.tsx
+++ src/common/components/TransparentLayer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
-const colors = ['rgba(0,0,0,0.80)', 'rgba(0,0,0,0.2)'];
+const colors = ['rgba(0,0,0,0.20)', 'rgba(0,0,0,0.8)'];
 
 export default function TransparentLayer() {
   return (
