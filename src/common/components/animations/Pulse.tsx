552ea751605d9c7e1a574cb82ea3dd7152f2fce0
Martin Santangelo <msantang78@gmail.com>
2021-10-14 12:36:27 -0300

    (chore) remove animatable as dependency and replace implementation with moti

---
+++ src/common/components/animations/Pulse.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from 'react';
+import { MotiProps, View as MotiView } from 'moti';
+import { ViewProps, ViewStyle } from 'react-native';
+import TopBarButtonTabBarItem from '../topbar-tabbar/TopBarButtonTabBarItem';
+
+const animation = {
+  from: {
+    scale: 1,
+  },
+
+  to: {
+    scale: 1.05,
+  },
+};
+
+/**
+ * Fade content from
+ */
+export const FadeFrom: FC<
+  ViewProps &
+    MotiProps<ViewStyle> & {
+      repeat?: number;
+    }
+> = ({ children, repeat = 4, ...props }) => {
+  const transition = {
+    repeat,
+    duration: 100,
+    delay: 30,
+  };
+
+  return (
+    <MotiView
+      {...props}
+      from={animation.from}
+      animate={animation.to}
+      transition={transition}>
+      {children}
+    </MotiView>
+  );
+};
+
+export default FadeFrom;
