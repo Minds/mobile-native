ebfad57a4bd58d35888d36f6373451269bafc878
Martin Santangelo <msantang78@gmail.com>
2022-01-11 18:26:47 -0300

    (fix) navigation issues from bottom sheets and tags

---
+++ src/navigation/withModalProvide.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+
+const withModalProvider = Component => props => (
+  <BottomSheetModalProvider>
+    <Component {...props} />
+  </BottomSheetModalProvider>
+);
+
+export default withModalProvider;
