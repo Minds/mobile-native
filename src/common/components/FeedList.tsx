38870b05b4f0912e93874489c3770c23fde208ff
Mani Shooshtari <mani@shooshtari.com>
2022-02-21 10:55:34 +0300

    fix activity indicator position and topbar border

--- src/common/components/FeedList.tsx
+++ src/common/components/FeedList.tsx
@@ -18,6 +18,7 @@ import type FeedStore from '../stores/FeedStore';
 import type ActivityModel from '../../newsfeed/ActivityModel';
 import ActivityIndicator from './ActivityIndicator';
 import MText from './MText';
+import { withSafeAreaInsets, EdgeInsets } from 'react-native-safe-area-context';
 
 type PropsType = {
   prepend?: React.ReactNode;
@@ -39,13 +40,14 @@ type PropsType = {
   afterRefresh?: () => void;
   onScroll?: (e: any) => void;
   refreshControlTintColor?: string;
+  insets?: EdgeInsets;
 };
 
 /**
  * News feed list component
  */
 @observer
-export default class FeedList<T> extends Component<PropsType> {
+class FeedList<T> extends Component<PropsType> {
   listRef?: FlatList<T>;
   cantShowActivity: string = '';
   viewOpts = {
@@ -171,13 +173,12 @@ export default class FeedList<T> extends Component<PropsType> {
         refreshing={feedStore.refreshing}
         onEndReached={this.loadMore}
         refreshControl={
-          Boolean(this.props.refreshControlTintColor) ? (
-            <RefreshControl
-              tintColor={this.props.refreshControlTintColor}
-              refreshing={feedStore.refreshing}
-              onRefresh={this.refresh}
-            />
-          ) : undefined
+          <RefreshControl
+            tintColor={this.props.refreshControlTintColor}
+            refreshing={feedStore.refreshing}
+            onRefresh={this.refresh}
+            progressViewOffset={(this.props.insets?.top || 0) / 1.25}
+          />
         }
         // onEndReachedThreshold={0}
         numColumns={feedStore.isTiled ? 3 : 1}
@@ -316,3 +317,5 @@ export default class FeedList<T> extends Component<PropsType> {
 const style = ThemedStyles.combine('flexContainer', 'bgPrimaryBackground');
 
 const footerStyle = ThemedStyles.combine('centered', 'padding3x');
+
+export default withSafeAreaInsets(FeedList);
