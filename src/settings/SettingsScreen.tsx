c6d0545dfd2b8fd48a4984c75ff4601e65ffc6ff
Martin Santangelo <msantang78@gmail.com>
2022-03-03 16:51:17 +0000

    Fix Billing settings

--- src/settings/SettingsScreen.tsx
+++ src/settings/SettingsScreen.tsx
@@ -1,26 +1,32 @@
-//@ts-nocheck
 import React, { useCallback } from 'react';
 import { ScrollView, View } from 'react-native';
 import { showMessage } from 'react-native-flash-message';
 import AuthService from '../auth/AuthService';
-import MenuItem from '../common/components/menus/MenuItem';
+import MenuItem, { MenuItemItem } from '../common/components/menus/MenuItem';
 import { isNetworkError } from '../common/services/api.service';
 import i18n from '../common/services/i18n.service';
 import openUrlService from '../common/services/open-url.service';
 import sessionService from '../common/services/session.service';
-import apiService from '../common/services/api.service';
+import apiService, { ApiResponse } from '../common/services/api.service';
 import ThemedStyles from '../styles/ThemedStyles';
 import { ScreenHeader, Screen } from '~/common/ui/screen';
-import { IS_REVIEW } from '~/config/Config';
+import { IS_REVIEW, IS_IOS } from '~/config/Config';
+
+interface HelpResponse extends ApiResponse {
+  url: string;
+}
 
 /**
  * Retrieves the link & jwt for zendesk and navigate to it.
  */
 const navigateToHelp = async () => {
   try {
-    const response = await apiService.get('api/v3/helpdesk/zendesk', {
-      returnUrl: 'true',
-    });
+    const response = await apiService.get<HelpResponse>(
+      'api/v3/helpdesk/zendesk',
+      {
+        returnUrl: 'true',
+      },
+    );
     if (response && response.url) {
       openUrlService.openLinkInInAppBrowser(unescape(response.url));
     }
@@ -42,6 +48,8 @@ const setDarkMode = () => {
   }
 };
 
+type Item = MenuItemItem & { screen?: string; params?: any };
+
 export default function ({ navigation }) {
   const theme = ThemedStyles.style;
 
@@ -58,7 +66,7 @@ export default function ({ navigation }) {
     [user],
   );
 
-  const firstSection = [
+  const firstSection: Array<Item> = [
     {
       title: i18n.t('settings.account'),
       screen: 'Account',
@@ -69,12 +77,15 @@ export default function ({ navigation }) {
       screen: 'Security',
       params: {},
     },
-    {
+  ];
+
+  if (!IS_IOS) {
+    firstSection.push({
       title: i18n.t('settings.billing'),
       screen: 'Billing',
       params: {},
-    },
-  ];
+    });
+  }
 
   if (!user.plus) {
     firstSection.push({
@@ -109,7 +120,7 @@ export default function ({ navigation }) {
     params: {},
   });
 
-  const secondSection = [
+  const secondSection: Array<Item> = [
     {
       title: i18n.t('boost'),
       screen: 'BoostConsole',
