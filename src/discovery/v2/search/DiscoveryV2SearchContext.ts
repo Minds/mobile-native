805ec696be4f7150a1b50fb77180e2da466e7558
Mani Shooshtari <879922-manishoo@users.noreply.gitlab.com>
2022-04-01 17:38:57 +0000

    fix discovery hashtags navigation and search debounce #4186

--- src/discovery/v2/search/DiscoveryV2SearchContext.ts
+++ src/discovery/v2/search/DiscoveryV2SearchContext.ts
@@ -1,8 +1,16 @@
 import React from 'react';
 import DiscoveryV2SearchStore from './DiscoveryV2SearchStore';
 
-const storesContext = React.createContext(new DiscoveryV2SearchStore());
+export const DiscoveryV2SearchStoreContext = React.createContext<
+  DiscoveryV2SearchStore | undefined
+>(undefined);
 
 export const useDiscoveryV2SearchStore = () => {
-  return React.useContext(storesContext);
+  const context = React.useContext(DiscoveryV2SearchStoreContext);
+
+  if (!context) {
+    throw new Error('DiscoveryV2SearchStoreContext unavailable');
+  }
+
+  return context;
 };
