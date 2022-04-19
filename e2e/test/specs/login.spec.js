8d1398c10c249013624b2592516cd117e1e537d8
Tanya <Tanya@TanyaMac>
2022-04-19 06:54:29 -0500

    POC - Login Test working on BrowsterStack iOS & Android Real

---
+++ e2e/test/specs/login.spec.js
@@ -0,0 +1,23 @@
+import LoginPage from '../pageobjects/login.page';
+
+describe('Login Page Test Cases', () => {
+  // Before Each method - updates settings.
+  beforeEach('Update App Settings...', async () => {
+    //driver.updateSettings({snapshotMaxDepth: 5000});
+  });
+
+  // Test Case to successfully login a valid user.
+  it('Should successfully login a valid user..', async () => {
+    await LoginPage.loginButton.waitForDisplayed();
+    await LoginPage.login(LoginPage.validUser, LoginPage.validPassword);
+    await LoginPage.homeButton.waitForDisplayed();
+    expect(LoginPage.homeButton.isDisplayed()).toBeTruthy(
+      'Login was not successful!',
+    );
+  });
+
+  // After All method - closes the mobile app.
+  afterAll('Close the browser...', async () => {
+    //browser.closeApp();
+  });
+});
