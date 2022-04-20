bc05c3cde421952869efb60ff819e292c1ada1b6
Mani Shooshtari <mani@shooshtari.com>
2022-04-20 16:39:00 +0300

    Support Roboto

--- src/common/components/friendly-captcha/html.ts
+++ src/common/components/friendly-captcha/html.ts
@@ -26,9 +26,20 @@ export default `
         --danger: #CA4A34;
       }
 
+      /* latin */
+      @font-face {
+        font-family: "Roboto";
+        font-style: normal;
+        font-weight: 400;
+        font-display: swap;
+        src: local("Roboto"), local("Roboto-Regular"), url("https://minds.com/static/assets/fonts/roboto/regular-latin.woff2") format("woff2");
+        src: local("Roboto"), local("Roboto-Regular"), url("https://minds.com/static/assets/fonts/roboto/regular-latin.woff") format("woff");
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+      }
+
       * {
         color: var(--primary-text);
-        font-family: Helvetica, sans-serif !important;
+        font-family: "Roboto", Helvetica, sans-serif !important;
       }
 
       body {
