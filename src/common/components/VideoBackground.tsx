80899473b6e97cf2f9efe5a52555418e84a55866
Martin Santangelo <msantang78@gmail.com>
2022-06-02 16:47:22 +0000

    Fix video player crash ios & update expo packages #4307

--- src/common/components/VideoBackground.tsx
+++ src/common/components/VideoBackground.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { StyleSheet } from 'react-native';
-import { Video } from 'expo-av';
+import { ResizeMode, Video } from 'expo-av';
 
 type PropsType = {
   source: any;
@@ -11,7 +11,7 @@ export default function VideoBackground(props: PropsType) {
   return (
     <Video
       source={props.source}
-      resizeMode={Video.RESIZE_MODE_COVER}
+      resizeMode={ResizeMode.COVER}
       isLooping
       isMuted={true}
       shouldPlay={true}
