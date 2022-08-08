3c927d27bc362600fcb87a7187db0176f6ad4269
Martin Santangelo <msantang78@gmail.com>
2022-08-08 16:26:14 -0300

    (feat) email in-feed notice

--- src/common/components/in-feed-notices/notices/EmailVerifyNotice.tsx
+++ src/common/components/in-feed-notices/notices/EmailVerifyNotice.tsx
@@ -1,13 +1,18 @@
-import { observer } from 'mobx-react-lite';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { observer } from 'mobx-react-lite';
+
 import i18nService from '~/common/services/i18n.service';
 import inFeedNoticesService from '~/common/services/in-feed.notices.service';
+import { hasVariation } from 'ExperimentsProvider';
+import sessionService from '~/common/services/session.service';
 import InFeedNotice from './BaseNotice';
 
 /**
  * Email Verify Notice
  */
 function EmailVerifyNotice() {
+  const navigation = useNavigation();
   if (!inFeedNoticesService.visible('verify-email')) {
     return null;
   }
@@ -17,7 +22,13 @@ function EmailVerifyNotice() {
       description={i18nService.t('inFeedNotices.verifyEmailDescription')}
       btnText={i18nService.t('inFeedNotices.verifyEmail')}
       iconName="warning"
-      onPress={() => console.log('resend email')}
+      onPress={() => {
+        if (hasVariation('minds-3055-email-codes')) {
+          sessionService.getUser().confirmEmailCode();
+        } else {
+          navigation.navigate('VerifyEmail');
+        }
+      }}
     />
   );
 }
