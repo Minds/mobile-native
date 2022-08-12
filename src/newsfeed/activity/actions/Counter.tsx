10929278fb293456135129c994a2270cae64f3be
Mani Shooshtari <mani@shooshtari.com>
2022-08-12 12:07:48 +0200

    (feat) animated counter

--- src/newsfeed/activity/actions/Counter.tsx
+++ src/newsfeed/activity/actions/Counter.tsx
@@ -1,8 +1,8 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 import { View } from 'react-native';
-import MText from '../../../common/components/MText';
-
-import abbrev from '../../../common/helpers/abbrev';
+import AnimatedNumbers from 'react-native-animated-numbers';
+import MText from '~/common/components/MText';
+import { abbrevWithUnit } from '../../../common/helpers/abbrev';
 import ThemedStyles from '../../../styles/ThemedStyles';
 
 type PropsType = {
@@ -11,31 +11,33 @@ type PropsType = {
   testID?: string;
 };
 
-/**
- * Counters
- */
-export default class Counter extends PureComponent<PropsType> {
-  /**
-   * Default Props
-   */
-  static defaultProps = {
-    size: 15,
-  };
+const Counter = ({ count }: PropsType) => {
+  const { number: abbrevatedNumber, unit } = abbrevWithUnit(count, 1);
+  const [d1, d2] = String(abbrevatedNumber).split('.');
 
-  /**
-   * Render
-   */
-  render(): React.ReactNode {
-    const { count, ...otherProps } = this.props;
+  return (
+    <View style={ThemedStyles.style.rowJustifyCenter}>
+      <AnimatedNumbers
+        animationDuration={500}
+        animateToNumber={Number(d1)}
+        fontStyle={textStyle}
+      />
+      {Boolean(d2) && (
+        <>
+          <MText style={textStyle}>.</MText>
+          <AnimatedNumbers
+            animationDuration={500}
+            animateToNumber={Number(d2)}
+            fontStyle={textStyle}
+          />
+        </>
+      )}
 
-    return (
-      <View style={ThemedStyles.style.columnAlignCenter}>
-        <MText style={textStyle} {...otherProps}>
-          {count > 0 ? abbrev(count, 1) : ''}
-        </MText>
-      </View>
-    );
-  }
-}
+      {Boolean(unit) && <MText style={textStyle}>{unit}</MText>}
+    </View>
+  );
+};
+
+export default memo(Counter);
 
 const textStyle = ThemedStyles.combine('colorIcon', 'fontM', 'fontMedium');
