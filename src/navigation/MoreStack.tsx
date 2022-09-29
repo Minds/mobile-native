7ce63575e7921d46cc71527e997961be497e703d
Martin Santangelo <msantang78@gmail.com>
2022-09-29 16:34:25 -0300

    (chore) settings behind FF

--- src/navigation/MoreStack.tsx
+++ src/navigation/MoreStack.tsx
@@ -9,6 +9,7 @@ import ThemedStyles from '~/styles/ThemedStyles';
 import Drawer from './Drawer';
 import i18n from '~/common/services/i18n.service';
 import { IS_IOS } from '~/config/Config';
+import { useFeature } from '@growthbook/growthbook-react';
 
 const MoreStack = createNativeStackNavigator<MoreStackParamList>();
 const hideHeader: NativeStackNavigationOptions = { headerShown: false };
@@ -19,6 +20,7 @@ const WalletOptions = () => ({
 });
 
 export default function () {
+  const supermindFeatureFlag = useFeature('mobile-supermind');
   const AccountScreenOptions = navigation => [
     {
       title: i18n.t('settings.accountOptions.1'),
@@ -84,16 +86,20 @@ export default function () {
           title: i18n.t('settings.billingOptions.2'),
           onPress: () => navigation.push('RecurringPayments'),
         },
-        {
-          title: 'Supermind',
-          onPress: () => navigation.push('SupermindSettingsScreen'),
-        },
+        supermindFeatureFlag.on
+          ? {
+              title: 'Supermind',
+              onPress: () => navigation.push('SupermindSettingsScreen'),
+            }
+          : null,
       ]
     : navigation => [
-        {
-          title: 'Supermind',
-          onPress: () => navigation.push('SupermindSettingsScreen'),
-        },
+        supermindFeatureFlag.on
+          ? {
+              title: 'Supermind',
+              onPress: () => navigation.push('SupermindSettingsScreen'),
+            }
+          : null,
       ];
 
   return (
