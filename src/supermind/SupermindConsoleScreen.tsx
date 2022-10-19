92532a24391cbdd1a5e5f2599ec982da16d3dbfa
Vali Nagacevschi <valentin@minds.com>
2022-10-19 20:58:17 +0100

    initial commit

--- src/supermind/SupermindConsoleScreen.tsx
+++ src/supermind/SupermindConsoleScreen.tsx
@@ -9,6 +9,7 @@ import TopbarTabbar, {
 } from '~/common/components/topbar-tabbar/TopbarTabbar';
 import i18n from '~/common/services/i18n.service';
 import { IconButton, Screen, ScreenHeader } from '~/common/ui';
+import { IS_IOS } from '~/config/Config';
 import ThemedStyles from '~/styles/ThemedStyles';
 import {
   SupermindOnboardingOverlay,
@@ -127,6 +128,6 @@ const mapRequests = items => SupermindRequestModel.createMany(items);
 
 const styles = ThemedStyles.create({
   onboardingOverlay: {
-    marginTop: 125,
+    marginTop: IS_IOS ? 125 : 70,
   },
 });
