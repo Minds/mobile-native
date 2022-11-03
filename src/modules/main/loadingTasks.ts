b08a1b6b3c150062d9a3c489ad6c5aea691906e7
Vali Nagacevschi <valentin@minds.com>
2022-11-03 10:18:19 +0000

    simplify modules aggregation

--- src/modules/main/loadingTasks.ts
+++
@@ -1,7 +0,0 @@
-export type TaskResult = [string, any];
-export type Task = () => Promise<TaskResult | void>;
-
-export const loadingMainTasks = (): Task[] => {
-  const mainTasks: Task[] = [async () => undefined];
-  return mainTasks;
-};
