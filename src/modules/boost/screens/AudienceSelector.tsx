eb118c36dc7c04080a6032abe481178bb7f42eb5
Mani Shooshtari <mani@shooshtari.com>
2022-12-08 14:14:46 +0100

    (fix) add fitScrollView

--- src/modules/boost/screens/AudienceSelector.tsx
+++ src/modules/boost/screens/AudienceSelector.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react';
 import React from 'react';
 import { showNotification } from '../../../../AppMessages';
+import FitScrollView from '../../../common/components/FitScrollView';
 import MenuItemOption from '../../../common/components/menus/MenuItemOption';
 import {
   B1,
@@ -43,48 +44,48 @@ function AudienceSelectorScreen({ navigation }: AudienceSelectorScreenProps) {
         back
         shadow
       />
+      <FitScrollView>
+        <Column align="centerBoth" vertical="XL2">
+          <H2>{t('Customize your audience')}</H2>
+          <B1 color="secondary">
+            {t('Choose the best audience for this boost.')}
+          </B1>
+        </Column>
 
-      <Column align="centerBoth" vertical="XL2">
-        <H2>{t('Customize your audience')}</H2>
-        <B1 color="secondary">
-          {t('Choose the best audience for this boost.')}
-        </B1>
-      </Column>
+        <HairlineRow />
 
-      <HairlineRow />
-
-      <Column vertical="L" horizontal="M" top="L2">
-        <H3 horizontal="L" bottom="S">
-          {t('Audience')}
-        </H3>
-        <MenuItemOption
-          title={t('Safe')}
-          subtitle={t(
-            'Content that is suitable if your kids or grandma saw it.',
-          )}
-          borderless
-          mode="radio"
-          selected={boostStore.audience === 'safe'}
-          onPress={() => boostStore.setAudience('safe')}
-        />
-        <MenuItemOption
-          title={t('Controversial')}
-          subtitle={t(
-            "Content that's political in nature or likely to be upsetting/controversial to a family-friendly audience.          ",
-          )}
-          borderless
-          mode="radio"
-          selected={boostStore.audience === 'mature'}
-          onPress={() => boostStore.setAudience('mature')}
-        />
-      </Column>
-      <Column flex />
+        <Column vertical="L" horizontal="M" top="L2">
+          <H3 horizontal="L" bottom="S">
+            {t('Audience')}
+          </H3>
+          <MenuItemOption
+            title={t('Safe')}
+            subtitle={t(
+              'Content that is suitable if your kids or grandma saw it.',
+            )}
+            borderless
+            mode="radio"
+            selected={boostStore.audience === 'safe'}
+            onPress={() => boostStore.setAudience('safe')}
+          />
+          <MenuItemOption
+            title={t('Controversial')}
+            subtitle={t(
+              "Content that's political in nature or likely to be upsetting/controversial to a family-friendly audience.",
+            )}
+            borderless
+            mode="radio"
+            selected={boostStore.audience === 'mature'}
+            onPress={() => boostStore.setAudience('mature')}
+          />
+        </Column>
+      </FitScrollView>
       <Button
         onPress={onNext}
         mode="solid"
         type="action"
         horizontal="L"
-        bottom="L2">
+        bottom="L">
         {t('Next')}
       </Button>
     </Screen>
