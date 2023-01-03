afdd7fd9d117897ee8f882bcf3f858a8e38f20bf
Martin Santangelo <msantang78@gmail.com>
2023-01-03 16:50:00 -0300

    (chore) remove unused method

--- src/settings/SettingsService.ts
+++ src/settings/SettingsService.ts
@@ -5,24 +5,6 @@ import api from '~/common/services/api.service';
  */
 class SettingsService {
   /**
-   * Load Categories
-   */
-  loadCategories() {
-    const rcategories: Array<any> = [];
-    return api.get<any>('api/v1/categories').then(categories => {
-      for (let id in categories.categories) {
-        rcategories.push({
-          id: id,
-          label: categories.categories[id],
-          selected: false,
-        });
-      }
-      rcategories.sort((a, b) => (a.label > b.label ? 1 : -1));
-      return rcategories;
-    });
-  }
-
-  /**
    * Get Settings
    */
   getSettings() {
