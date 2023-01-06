84fbeac9659eb910fbee1d982c57b2f1e4337c47
Mani Shooshtari <mani@shooshtari.com>
2023-01-06 16:19:34 +0100

    (fix) date format

--- src/modules/boost/boost-console/components/v3/BoostActionBar.tsx
+++ src/modules/boost/boost-console/components/v3/BoostActionBar.tsx
@@ -1,12 +1,8 @@
 import { observer } from 'mobx-react';
 import React, { ReactElement } from 'react';
-import { View } from 'react-native';
-import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import type UserStore from '~/auth/UserStore';
-import MText from '~/common/components/MText';
 import i18n from '~/common/services/i18n.service';
 import { B1, Button, Column, Row } from '~/common/ui';
-import ThemedStyles from '~/styles/ThemedStyles';
 import { showNotification } from '../../../../../../AppMessages';
 import { confirm } from '../../../../../common/components/Confirm';
 import { useTranslation } from '../../../locales';
@@ -23,18 +19,7 @@ function BoostActionBar({ boost }: BoostActionBarProps) {
   const boostConsoleStore = useBoostConsoleStore();
   const { t } = useTranslation();
   const revokable = boost.boost_status === BoostStatus.PENDING;
-
-  function renderTime() {
-    const date = i18n.date(boost.created_timestamp * 1000, 'friendly');
-    return (
-      <View style={ThemedStyles.style.flexColumnCentered} key="time">
-        <MCIcon name="clock" size={20} style={styles.icon} />
-        <MText style={styles.value} numberOfLines={1}>
-          {date}
-        </MText>
-      </View>
-    );
-  }
+  const date = i18n.date(boost.created_timestamp * 1000, 'friendly');
 
   function renderActions() {
     let buttons: ReactElement[] = [];
@@ -89,7 +74,7 @@ function BoostActionBar({ boost }: BoostActionBarProps) {
           <B1 color="secondary">{boost.entity?.impressions ?? ''}</B1>
         </Column>
         <Column flex>
-          <B1 color="secondary">{renderTime()}</B1>
+          <B1 color="secondary">{date}</B1>
         </Column>
       </Row>
       {renderActions()}
@@ -98,21 +83,3 @@ function BoostActionBar({ boost }: BoostActionBarProps) {
 }
 
 export default observer(BoostActionBar);
-
-const styles = ThemedStyles.create({
-  container: [
-    'bcolorBaseBackground',
-    {
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'stretch',
-      padding: 4,
-      paddingBottom: 16,
-    },
-  ],
-  icon: ['marginBottom', 'colorSecondaryText'],
-  value: {
-    fontSize: 11,
-    marginTop: 4,
-  },
-});
