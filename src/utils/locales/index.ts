4ef2ba60055f889ec12e86787cf4daa8dee64e80
Martin Santangelo <msantang78@gmail.com>
2023-01-09 23:22:29 -0300

    (fix) i18next should use user's language

--- src/utils/locales/index.ts
+++ src/utils/locales/index.ts
@@ -1,15 +1,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { storages } from '~/common/services/storage/storages.service';
 
 i18n.use(initReactI18next).init({
   resources: {
     en: {},
   },
   lng: 'en',
-  keySeparator: false,
+  keySeparator: '.',
   interpolation: {
     escapeValue: false,
   },
 });
+
+const language = storages.app.getString('locale');
+if (language) {
+  i18n.changeLanguage(language);
+}
 
 export default i18n;
