46eff7fc9ec7bf415eedbb7edc239ba4a1421f01
Martin Santangelo <msantang78@gmail.com>
2023-01-30 14:15:28 -0300

    Fix spec tests

--- __tests__/common/services/attachment.service.js
+++ __tests__/common/services/attachment.service.js
@@ -12,16 +12,18 @@ describe('Attachment service', () => {
   beforeEach(() => {
     api.upload.mockClear();
     api.delete.mockClear();
+    imagePicker.launchCamera.mockClear();
+    imagePicker.launchImageLibrary.mockClear();
   });
 
   it('should call attach media endpoint', async () => {
     let media = {
-        uri: 'uri',
-        path: 'path',
-        type: 'type',
-        filename: 'filename'
-    }
-    const apiResponse = {loaded: 10, total:200};
+      uri: 'uri',
+      path: 'path',
+      type: 'type',
+      filename: 'filename',
+    };
+    const apiResponse = { loaded: 10, total: 200 };
 
     api.upload.mockResolvedValue(apiResponse);
 
@@ -31,17 +33,15 @@ describe('Attachment service', () => {
     // call api upload one time
     expect(api.upload.mock.calls.length).toEqual(1);
     expect(api.upload.mock.calls[0][0]).toEqual('api/v1/media/');
-
   });
-
 
   it('should call attach media endpoint withouth filename should fallback', async () => {
     let media = {
-        uri: 'uri',
-        path: 'path',
-        type: 'type',
-    }
-    const apiResponse = {loaded: 10, total:200};
+      uri: 'uri',
+      path: 'path',
+      type: 'type',
+    };
+    const apiResponse = { loaded: 10, total: 200 };
 
     api.upload.mockResolvedValue(apiResponse);
 
@@ -52,69 +52,58 @@ describe('Attachment service', () => {
     media.name = 'test';
     expect(api.upload.mock.calls[0][1]).toEqual(media);
     expect(api.upload.mock.calls[0][0]).toEqual('api/v1/media/');
-
   });
 
-  it('should call launchCamera', async () => {
-    const apiResponse = {uri: 'aaaa', 'path': 'path metheny'};
+  it('should call launchCamera for video', async () => {
+    const apiResponse = { uri: 'fakeuri', path: 'fakeuri' };
 
     imagePicker.launchCamera.mockResolvedValue(apiResponse);
 
     // call tested method
     const res = await service.video();
 
-
     expect(res).toEqual({
-        uri: 'aaaa',
-        path: 'path metheny',
-        type: 'video/mp4',
-        fileName: 'image.mp4'
+      uri: 'fakeuri',
+      path: 'fakeuri',
+      type: 'video/mp4',
+      fileName: 'image.mp4',
     });
-
   });
 
   it('should call launchCamera with null response and return', async () => {
-
     imagePicker.launchCamera.mockResolvedValue(null);
 
     // call tested method
     const res = await service.video();
     expect(res).toEqual(null);
-
   });
 
-
-  it('should call launchCamera', async () => {
-    const apiResponse = {uri: 'aaaa', 'path': 'path metheny'};
+  it('should call launchCamera for image', async () => {
+    const apiResponse = { uri: 'fakeuri', path: 'fakeuri' };
 
     imagePicker.launchCamera.mockResolvedValue(apiResponse);
 
     // call tested method
     const res = await service.photo();
 
-
     expect(res).toEqual({
-        uri: 'aaaa',
-        path: 'path metheny',
-        type: 'image/jpeg',
-        fileName: 'image.jpg'
+      uri: 'fakeuri',
+      path: 'fakeuri',
+      type: 'image/jpeg',
+      fileName: 'image.jpg',
     });
-
   });
 
   it('should call launchCamera with null response and return', async () => {
-
     imagePicker.launchCamera.mockResolvedValue(null);
 
     // call tested method
     const res = await service.photo();
     expect(res).toEqual(null);
-
   });
-
 
   it('should call delete media endpoint', async () => {
-    const apiResponse = {success:true};
+    const apiResponse = { success: true };
 
     api.delete.mockResolvedValue(apiResponse);
 
@@ -123,51 +112,28 @@ describe('Attachment service', () => {
 
     expect(api.delete.mock.calls.length).toEqual(1);
     expect(api.delete.mock.calls[0][0]).toEqual('api/v1/media/1111');
-
   });
 
   it('should call launchImageLibrary', async () => {
-    const apiResponse = {uri: 'aaaa', 'path': 'path metheny', width: 100};
+    const apiResponse = { uri: 'fakeuri', path: 'fakeuri', width: 100 };
 
     imagePicker.launchImageLibrary.mockResolvedValue(apiResponse);
 
     // call tested method
     const res = await service.gallery();
 
-
     expect(res).toEqual({
-        uri: 'aaaa',
-        path: 'path metheny',
-        width: 100
+      uri: 'fakeuri',
+      path: 'fakeuri',
+      width: 100,
     });
-
-  });
-
-  it('should call launchImageLibrary', async () => {
-    const apiResponse = {uri: 'aaaa', 'path': 'path metheny'};
-
-    imagePicker.launchImageLibrary.mockResolvedValue(apiResponse);
-
-    // call tested method
-    const res = await service.gallery();
-
-
-    expect(res).toEqual({
-        uri: 'aaaa',
-        path: 'path metheny',
-        type: 'video/mp4'
-    });
-
   });
 
   it('should call launchImageLibrary with null response and return', async () => {
-
     imagePicker.launchImageLibrary.mockResolvedValue(null);
 
     // call tested method
     const res = await service.gallery();
     expect(res).toEqual(null);
-
   });
-
 });
