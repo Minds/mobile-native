47064a5a0b519ade9e4a47c74119402d04977207
Mani Shooshtari <mani@shooshtari.com>
2023-03-09 13:29:53 +0100

    (feat) new list items

--- packages/design-system/src/View.ts
+++ packages/design-system/src/View.ts
@@ -1,9 +1,19 @@
 import { styled, Stack } from '@tamagui/core';
 
 export const Layout = styled(Stack, {
-  tag: 'layout',
+  tag: 'Layout',
   backgroundColor: '$background',
   flex: 1,
+});
+
+export const Row = styled(Stack, {
+  tag: 'Row',
+  fd: 'row',
+});
+
+export const Column = styled(Stack, {
+  tag: 'Row',
+  fd: 'column',
 });
 
 export { Stack as View };
--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -11,5 +11,6 @@ export * from './Text';
 export * from './View';
 export * from './Button';
 export * from './TabBar';
+export * from './ListItem';
 export * from './IconButton';
 export * from './Avatar';
