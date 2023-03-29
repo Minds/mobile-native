91464310d87ecdba2085731c0c6bce4bc3816dce
Mani Shooshtari <mani@shooshtari.com>
2023-03-29 16:08:04 +0200

    (refactor) use exports

--- src/modules/boost/index.ts
+++ src/modules/boost/index.ts
@@ -1,13 +1,5 @@
-import BoostComposerStack from './boost-composer/navigator';
-import BoostConsoleScreen from './boost-console/screens/BoostConsoleScreen';
-import SingleBoostConsoleScreen from './boost-console/screens/SingleBoostConsoleScreen';
-import boostedContentService from './services/boosted-content.service';
-import { cleanBoosts } from './utils/clean-boosts';
-
-export {
-  BoostComposerStack,
-  BoostConsoleScreen,
-  SingleBoostConsoleScreen,
-  boostedContentService,
-  cleanBoosts,
-};
+export { default as BoostComposerStack } from './boost-composer/navigator';
+export { default as BoostConsoleScreen } from './boost-console/screens/BoostConsoleScreen';
+export { default as SingleBoostConsoleScreen } from './boost-console/screens/SingleBoostConsoleScreen';
+export { default as boostedContentService } from './services/boosted-content.service';
+export { cleanBoosts } from './utils/clean-boosts';
