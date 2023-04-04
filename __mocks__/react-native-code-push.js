8b87e2e122a9aff990950f0361a682332e0e4b9f
Martin Santangelo <msantang78@gmail.com>
2023-04-04 12:23:21 -0300

    Fix spec tests on master Changelog: other

--- __mocks__/react-native-code-push.js
+++ __mocks__/react-native-code-push.js
@@ -7,4 +7,11 @@ codePush.InstallMode = {
   ON_NEXT_SUSPEND: 0,
 };
 
+codePush.SyncStatus = {
+  CHECKING_FOR_UPDATE: 0,
+  DOWNLOADING_PACKAGE: 1,
+  INSTALLING_UPDATE: 2,
+  SYNC_IN_PROGRESS: 3,
+};
+
 export default codePush;
