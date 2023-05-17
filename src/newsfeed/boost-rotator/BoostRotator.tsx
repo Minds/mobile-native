8eea52a9d3f4086c0d4776de417742ec186fac81
Martin Santangelo <msantang78@gmail.com>
2023-05-17 15:48:58 -0300

    Render a place holder in the boost rotator until data is loaded Changelog: added

--- src/newsfeed/boost-rotator/BoostRotator.tsx
+++ src/newsfeed/boost-rotator/BoostRotator.tsx
@@ -1,18 +1,14 @@
 import { observer } from 'mobx-react';
 import sessionService from '~/common/services/session.service';
-import {
-  useBoostRotatorStore,
-  withBoostRotatorStore,
-} from './boost-rotator.store';
+import { withBoostRotatorStore } from './boost-rotator.store';
 import BoostRotatorCarousel from './components/BoostRotatorCarousel';
 import BoostRotatorHeader from './components/BoostRotatorHeader';
 import BoostRotatorPageIndicator from './components/BoostRotatorPageIndicator';
 
 function BoostRotator() {
-  const boostRotatorStore = useBoostRotatorStore();
   const user = sessionService.getUser();
 
-  if (user.disabled_boost || !boostRotatorStore.activites.length) {
+  if (user.disabled_boost) {
     return null;
   }
 
