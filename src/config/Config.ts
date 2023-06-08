684b3590e710f14ffa6e4b5bdf973aaa9f595e24
Vali Nagacevschi <valentin@minds.com>
2023-06-08 10:36:56 +0100

    to trigger CI/CD

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -8,6 +8,7 @@ import CodePush, { CodePushOptions } from 'react-native-code-push';
 
 export const IS_IOS = Platform.OS === 'ios';
 export const IS_IPAD = (Platform as PlatformIOSStatic).isPad;
+
 export const ONCHAIN_ENABLED = false;
 export const PRO_PLUS_SUBSCRIPTION_ENABLED = !IS_IOS;
 
