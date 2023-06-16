4eb059b8879f53107ce451800bdfc50eb1f60c77
Martin Santangelo <msantang78@gmail.com>
2023-06-16 14:23:57 -0300

    Add supermind feed to the supermind console Changelog: added

--- src/common/components/in-feed-notices/notices/PendingSupermindNotice.tsx
+++ src/common/components/in-feed-notices/notices/PendingSupermindNotice.tsx
@@ -9,12 +9,19 @@ import { NoticeProps } from '.';
 /**
  * Pending Supermind Notice
  */
-function PendingSupermindNotice({ name }: NoticeProps) {
+function PendingSupermindNotice({
+  name,
+  onPress,
+}: NoticeProps & { onPress?: () => void }) {
   const navigation = useNavigation();
 
   // on button press
-  const onPress = () => {
-    navigation.navigate('More', { screen: 'SupermindConsole', initial: false });
+  const onPressDefault = () => {
+    navigation.navigate('More', {
+      screen: 'SupermindConsole',
+      initial: false,
+      tab: 'inbound',
+    });
   };
 
   return (
@@ -24,7 +31,7 @@ function PendingSupermindNotice({ name }: NoticeProps) {
       description={i18nService.t('inFeedNotices.pendingSupermindDescription')}
       btnText={i18nService.t('inFeedNotices.pendingSupermindButton')}
       iconName="info-outline"
-      onPress={onPress}
+      onPress={onPress || onPressDefault}
     />
   );
 }
