9b93392e622122a6bdc75a147e36c080575c82f7
Vali Nagacevschi <valentin@minds.com>
2023-07-24 18:15:13 +0100

    revert changes

--- src/settings/screens/blocked/createBlockedChannelsStore.ts
+++ src/settings/screens/blocked/createBlockedChannelsStore.ts
@@ -3,20 +3,17 @@ import FeedStore from '~/common/stores/FeedStore';
 
 const createBlockedChannelsStore = () => {
   const store = {
-    loading: false,
     feedStore: new FeedStore<UserModel>(true),
     async loadList(refresh = false) {
       if (refresh) {
         this.feedStore.clear();
       }
-      this.loading = true;
-      await this.feedStore
+      this.feedStore
         .setEndpoint('api/v1/block')
         .setAsActivities(false)
         .setLimit(12)
         .noSync()
         .fetchRemoteOrLocal(refresh);
-      this.loading = false;
       return;
     },
   };
