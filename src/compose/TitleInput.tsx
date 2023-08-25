98685be2a9e6d53ae1f22fbce2c89d5190c717f6
Mani Shooshtari <mani@shooshtari.com>
2023-08-25 14:22:44 +0200

    Composer: Improved the title input
    Changelog: changed

--- src/compose/TitleInput.tsx
+++ src/compose/TitleInput.tsx
@@ -1,77 +1,41 @@
-import React, { useState, useCallback } from 'react';
-import { View } from 'react-native';
-import Icon from 'react-native-vector-icons/SimpleLineIcons';
-
-import ThemedStyles, { useMemoStyle } from '../styles/ThemedStyles';
-import i18nService from '../common/services/i18n.service';
 import { observer } from 'mobx-react';
+import React from 'react';
+import { View } from 'react-native';
 import TextInput from '../common/components/TextInput';
-import MText from '../common/components/MText';
+import i18nService from '../common/services/i18n.service';
+import ThemedStyles from '../styles/ThemedStyles';
 
 /**
  * Title input
  * @param {Object} props
  */
 export default observer(function TitleInput(props) {
-  const [open, setOpen] = useState(Boolean(props.store.title));
-  const onPress = useCallback(() => {
-    setOpen(old => {
-      if (old === true) {
-        props.store.setTitle('');
-      }
-      return !old;
-    });
-  }, [setOpen, props]);
-
-  const theme = ThemedStyles.style;
   return (
-    <View style={open ? container : containerClosed}>
-      <View
-        style={useMemoStyle(
-          [
-            'fullWidth',
-            'rowJustifyEnd',
-            'alignCenter',
-            'paddingRight',
-            open ? {} : { width: 80 },
-          ],
-          [open],
-        )}>
-        <Icon
-          name={open ? 'minus' : 'plus'}
-          size={16}
-          style={theme.colorTertiaryText}
-          onPress={onPress}
-        />
-        <MText style={titleStyle} onPress={onPress}>
-          Title
-        </MText>
-      </View>
-      {open && (
-        <TextInput
-          style={textInputStyle}
-          placeholder={i18nService.t('title')}
-          placeholderTextColor={ThemedStyles.getColor('TertiaryText')}
-          onChangeText={props.store.setTitle}
-          textAlignVertical="top"
-          value={props.store.title}
-          multiline={false}
-          selectTextOnFocus={false}
-          underlineColorAndroid="transparent"
-          testID="PostTitleInput"
-        />
-      )}
+    <View style={container}>
+      <TextInput
+        autoFocus
+        style={textInputStyle}
+        placeholder={i18nService.t('title')}
+        placeholderTextColor={ThemedStyles.getColor('TertiaryText')}
+        onChangeText={props.store.setTitle}
+        textAlignVertical="top"
+        value={props.store.title}
+        multiline
+        selectTextOnFocus={false}
+        underlineColorAndroid="transparent"
+        testID="PostTitleInput"
+      />
     </View>
   );
 });
 
-const textInputStyle = ThemedStyles.combine('colorPrimaryText', 'fontXXL');
-const container = ThemedStyles.combine('fullWidth');
-const containerClosed = ThemedStyles.combine('fullWidth', {
-  alignItems: 'flex-end',
-});
-const titleStyle = ThemedStyles.combine(
-  'fontL',
-  'colorTertiaryText',
-  'paddingHorizontal2x',
+const textInputStyle = ThemedStyles.combine(
+  'colorPrimaryText',
+  'fontXXL',
+  'bold',
+);
+const container = ThemedStyles.combine(
+  'fullWidth',
+  'marginTop2x',
+  'marginBottom',
 );
---
+++ src/compose/TitleToggle.tsx
@@ -0,0 +1,36 @@
+import { observer } from 'mobx-react';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import { Button } from '../common/ui';
+import ThemedStyles from '../styles/ThemedStyles';
+
+/**
+ * Title toggle
+ * @param {Object} props
+ */
+function TitleToggle(props) {
+  const theme = ThemedStyles.style;
+
+  const onPress = useCallback(() => {
+    props.store.toggleTitle();
+  }, [props.store]);
+
+  return (
+    <Button
+      size="pill"
+      mode="flat"
+      color="tertiary"
+      onPress={onPress}
+      icon={
+        <Icon
+          name={props.store.isTitleOpen ? 'minus' : 'plus'}
+          size={16}
+          style={theme.colorTertiaryText}
+        />
+      }>
+      Title
+    </Button>
+  );
+}
+
+export default observer(TitleToggle);
