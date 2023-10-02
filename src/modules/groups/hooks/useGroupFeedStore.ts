f43aaf4647e7e194dfd7459554c1592f36443e81
Martin Santangelo <msantang78@gmail.com>
2023-10-02 15:28:34 -0300

    Top feed added to groups screen Changelog: added

--- src/modules/groups/hooks/useGroupFeedStore.ts
+++ src/modules/groups/hooks/useGroupFeedStore.ts
@@ -3,9 +3,12 @@ import { useEffect } from 'react';
 import FeedStore from '~/common/stores/FeedStore';
 import type GroupModel from '~/groups/GroupModel';
 
+export type GroupFeedAlgorithms = 'latest' | 'top';
+
 export function useGroupFeedStore(group: GroupModel) {
   const store = useLocalStore(() => ({
     feed: new FeedStore(true),
+    feedTop: new FeedStore(true),
     search: '',
     showSearch: false,
     toggleSearch() {
@@ -19,20 +22,33 @@ export function useGroupFeedStore(group: GroupModel) {
 
       const params = value
         ? {
-            period: '1y',
+            period: 'all',
             all: 1,
             query: this.search,
             sync: 1,
           }
-        : {};
+        : { algorithm: 'latest' };
       this.feed.clear().setParams(params).fetchRemoteOrLocal();
     },
   }));
 
   useEffect(() => {
     store.feed.getMetadataService()?.setSource('feed/groups').setMedium('feed');
+    store.feedTop
+      .getMetadataService()
+      ?.setSource('top-feed/groups')
+      .setMedium('feed');
+
     store.feed
       .setEndpoint(`api/v2/feeds/container/${group.guid}/activities`)
+      .setInjectBoost(Boolean(group.show_boosts))
+      .setParams({ algorithm: 'latest' })
+      .setLimit(12)
+      .fetchRemoteOrLocal();
+
+    store.feedTop
+      .setEndpoint(`api/v2/feeds/container/${group.guid}/activities`)
+      .setParams({ algorithm: 'top' })
       .setInjectBoost(Boolean(group.show_boosts))
       .setLimit(12)
       .fetchRemoteOrLocal();
