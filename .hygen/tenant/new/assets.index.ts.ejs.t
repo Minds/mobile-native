9c067eeba4cfbc839b013c0bbe523534c53f5353
Vali Nagacevschi <valentin@minds.com>
2023-10-17 14:48:59 +0100

    refactor tenants assets

--- .hygen/tenant/new/assets.index.ts.ejs.t
+++
@@ -1,4 +0,0 @@
----
-to: "./assets/<%=camelName%>/images/.touch"
-sh: "<%= `cp ${absPath}/assets/minds/images/* ${absPath}/assets/${camelName}/images`%>"
----
--- assets/index.ts
+++ assets/index.ts
@@ -1,9 +1,9 @@
 export default {
-  ICON: require('./minds/images/icon_logo.png'),
-  COMPOSE: require('./minds/images/compose.png'),
-  EMPTY_FEED: require('./minds/images/emptyFeed.png'),
-  LOGO: require('./minds/images/logo.png'),
-  LOGO_WHITE: require('./minds/images/logo_white.png'),
-  LOGO_MONO: require('./minds/images/logo_mono.png'),
-  LOGO_MONO_WHITE: require('./minds/images/logo_mono_white.png'),
+  ICON: require('./images/icon_logo.png'),
+  COMPOSE: require('./images/compose.png'),
+  EMPTY_FEED: require('./images/emptyFeed.png'),
+  LOGO: require('./images/logo.png'),
+  LOGO_WHITE: require('./images/logo_white.png'),
+  LOGO_MONO: require('./images/logo_mono.png'),
+  LOGO_MONO_WHITE: require('./images/logo_mono_white.png'),
 } as const;
