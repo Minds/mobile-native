16a7c5831901a5bb4a95248913d466c7eebe1465
Vali Nagacevschi <valentin@minds.com>
2023-12-01 16:58:32 +0000

    revert email-confirmation.service

--- src/common/services/email-confirmation.service.ts
+++ src/common/services/email-confirmation.service.ts
@@ -5,9 +5,12 @@ import logService from './log.service';
 class EmailConfirmationService {
   async send() {
     try {
-      const response = await apiService.post('api/v3/email/send', {});
+      const response = await apiService.post(
+        'api/v2/email/confirmation/resend',
+        {},
+      );
 
-      return Boolean(response && response.status === 'success');
+      return Boolean(response && response.sent);
     } catch (err) {
       logService.exception('[EmailConfirmationService] send', err);
       return false;
