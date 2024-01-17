77a21623231be9f74c276eb22f6528b0f5d0c2b5
Martin Santangelo <msantang78@gmail.com>
2024-01-17 10:01:20 -0300

    wip

--- tasks/setup-tenant.js
+++ tasks/setup-tenant.js
@@ -1,123 +1,30 @@
-const fs = require('fs');
-const fse = require('fs-extra');
-const args = process.argv.slice(2);
+const { request } = require('graphql-request');
 
-const tenant = args[0];
-const preview = args[1] === '--preview';
+const query = `
+query GetMobileConfig($tenantId: Int!) {
+  appReadyMobileConfig(tenantId: $tenantId) {
+    APP_NAME
+    TENANT_ID
+    APP_HOST
+    APP_SPLASH_RESIZE
+    ACCENT_COLOR_LIGHT
+    ACCENT_COLOR_DARK
+    WELCOME_LOGO
+    THEME
+    API_URL
+    assets {
+      key
+      value
+    }
+    __typename
 
-if (tenant) {
-  console.log(`Setting up tenant ${tenant}`);
-  prepareTenant(tenant);
-  // Implement tenant setup here
-} else {
-  console.log('No tenant provided, default to minds');
+  }
 }
+`;
 
-function prepareTenant(companyID) {
-  const command =
-    'npx eas-cli build --platform android --profile demo --non-interactive --json --no-wait';
-  // const command = `eas build --platform android --profile demo --non-interactive --json --no-wait --local`;
-  // const Tenant = require(`../tenants/${companyID}/tenant.json`);
-  fs.copyFileSync(`../tenants/${companyID}/tenant.json`, './tenant.json');
+const url =
+  'https://c81e728d9d4c2f636f067f89cc14862c.bens-networks.oke.minds.io/api/graphql'; // replace with your GraphQL endpoint
 
-  if (fs.existsSync(`../tenants/${companyID}/icon.png`)) {
-    fs.copyFileSync(
-      `../tenants/${companyID}/icon.png`,
-      './assets/images/icon.png',
-    );
-  } else {
-    fs.copyFileSync(
-      `../tenants/${companyID}/square.png`,
-      './assets/images/icon.png',
-    );
-  }
-
-  fs.copyFileSync(
-    `../tenants/${companyID}/square.png`,
-    './assets/images/logo_square.png',
-  );
-
-  fs.copyFileSync(
-    `../tenants/${companyID}/horizontal.png`,
-    './assets/images/logo_horizontal.png',
-  );
-  fs.copyFileSync(
-    `../tenants/${companyID}/horizontal.png`,
-    './assets/images/logo_horizontal_dark.png',
-  );
-
-  fs.copyFileSync(
-    `../tenants/${companyID}/splash.png`,
-    './assets/images/splash.png',
-  );
-
-  if (fs.existsSync(`../tenants/${companyID}/adaptive-icon.png`)) {
-    fs.copyFileSync(
-      `../tenants/${companyID}/adaptive-icon.png`,
-      './assets/images/adaptive-icon.png',
-    );
-  }
-
-  if (preview) {
-    console.log('Preview mode, overriding tenant.json properties...');
-    fs.readFile('./preview/tenant/tenant.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      const previewJson = JSON.parse(data);
-
-      fs.readFile('./tenant.json', 'utf8', (errTenant, dataTenant) => {
-        if (errTenant) {
-          console.error(errTenant);
-          return;
-        }
-
-        const tenantJson = JSON.parse(dataTenant);
-
-        tenantJson.EAS_PROJECT_ID = previewJson.EAS_PROJECT_ID;
-        tenantJson.APP_SLUG = previewJson.APP_SLUG;
-        tenantJson.IS_PREVIEW = true;
-
-        fs.writeFile(
-          './tenant.json',
-          JSON.stringify(tenantJson, null, 2),
-          'utf8',
-          err => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-            console.log('tenant.json updated successfully');
-          },
-        );
-      });
-    });
-
-    fse.copy('./preview/tenant/patches', './patches', err => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log('Preview patches copied successfully');
-    });
-  }
-
-  // const childProcess = exec(command, (error, stdout, stderr) => {
-  //   if (error) {
-  //     console.error(`Error building for ${company.name}: ${error}`);
-  //   } else {
-  //     console.log(`Build for ${company.name} completed successfully.`);
-  //     console.log(stdout);
-  //   }
-  // });
-
-  // childProcess.stdout.on('data', data => {
-  //   console.log(data);
-  // });
-
-  // childProcess.stderr.on('data', data => {
-  //   console.error(data);
-  // });
-}
+request(url, query, { tenantId: 2 })
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
