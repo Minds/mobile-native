543890b79b9c4c9beca1df1b53396c1729f78204
Vali Nagacevschi <valentin@minds.com>
2024-01-18 22:18:11 +0000

    fix android cookies

--- src/auth/CookieService.ts
+++ src/auth/CookieService.ts
@@ -1,23 +1,35 @@
 import CookieManager, { Cookie, Cookies } from '@react-native-cookies/cookies';
-import { MINDS_URI } from '~/config/Config';
+import { IS_IOS, MINDS_URI } from '~/config/Config';
+const ANDROID = !IS_IOS;
 
 class CookieService {
   async get() {
     return CookieManager.get(MINDS_URI);
   }
-  async set(cookie: Cookie) {
-    return CookieManager.set(MINDS_URI, { path: '/', ...cookie });
+  async set(cookie: Cookie, flush: boolean = true) {
+    return CookieManager.set(MINDS_URI, { path: '/', ...cookie }).then(
+      result => {
+        ANDROID && flush && CookieManager.flush();
+        return result;
+      },
+    );
   }
   async getCookie(key: string) {
     return (await CookieManager.get(MINDS_URI))?.[key]?.value;
   }
   async setFromCookies(cookies: Cookies) {
     return Promise.all(
-      Object.keys(cookies).map(async key => this.set(cookies[key])),
-    );
+      Object.keys(cookies).map(async key => this.set(cookies[key], false)),
+    ).then(result => {
+      ANDROID && CookieManager.flush();
+      return result;
+    });
   }
   async clearByName(name: string) {
-    return CookieManager.clearByName(MINDS_URI, name);
+    return CookieManager.clearByName(MINDS_URI, name).then(result => {
+      ANDROID && CookieManager.flush();
+      return result;
+    });
   }
 }
 
