f95dad36d426104983973075a726b6fdbcfaf164
Vali Nagacevschi <valentin@minds.com>
2024-02-07 12:46:00 +0000

    replace confirm

--- src/settings/screens/MindsProScreen.tsx
+++ src/settings/screens/MindsProScreen.tsx
@@ -1,14 +1,12 @@
 import { B1, Button, Screen } from '~/common/ui';
 import subscriptionProService from '~/common/services/subscription.pro.service';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import sessionService from '~/common/services/session.service';
 import { useNavigation } from '@react-navigation/native';
-import BottomSheetModal from '~/common/components/bottom-sheet/BottomSheetModal';
-import { BottomSheetButton } from '~/common/components/bottom-sheet';
+import { confirm } from '~/common/components/Confirm';
 
 export default function () {
-  const { show, shown, ref, close, cancelSubscription, expiryString } =
-    useCancelProSubscription();
+  const { cancelSubscription, expiryString } = useCancelProSubscription();
   return (
     <Screen>
       <B1 horizontal="XL2">Manage your subscription</B1>
@@ -18,25 +16,10 @@ export default function () {
         horizontal="XL2"
         vertical="XL2"
         spinner
-        onPress={show}>
+        onPress={cancelSubscription}>
         Cancel Subscription
       </Button>
       <B1 horizontal="XL2">You still have Minds Pro until {expiryString}</B1>
-      {shown && (
-        <BottomSheetModal ref={ref} autoShow>
-          <B1 horizontal="XL" align="center">
-            {'Are you sure you want to cancel your\nPro subscription?'}
-          </B1>
-          <BottomSheetButton
-            action
-            solid
-            text="Yes, I'm sure"
-            onPress={cancelSubscription}
-          />
-
-          <BottomSheetButton text={'No'} onPress={close} />
-        </BottomSheetModal>
-      )}
     </Screen>
   );
 }
@@ -45,17 +28,6 @@ const useCancelProSubscription = () => {
   const navigation = useNavigation();
   const user = sessionService.getUser();
   const [expiryString, setExpiryString] = useState('');
-  const [shown, setShown] = useState(false);
-
-  const ref = useRef<any>();
-
-  const close = useCallback(() => {
-    ref.current?.dismiss();
-  }, []);
-
-  const show = useCallback(() => {
-    shown ? ref.current?.present() : setShown(true);
-  }, [shown]);
 
   useEffect(() => {
     subscriptionProService
@@ -64,16 +36,20 @@ const useCancelProSubscription = () => {
   }, [expiryString]);
 
   const cancelSubscription = async () => {
-    await subscriptionProService.disable();
-    user.togglePro();
-    navigation.goBack();
+    if (
+      await confirm({
+        title: 'Confirm Cancellation',
+        actionText: 'Yes, I’m sure',
+        description: 'Are you sure you want to cancel your Pro subscription?',
+      })
+    ) {
+      await subscriptionProService.disable();
+      user.togglePro();
+      navigation.goBack();
+    }
   };
 
   return {
-    shown,
-    show,
-    ref,
-    close,
     cancelSubscription,
     expiryString,
   };
