55cf503c22bc055e9ca7109a77dd3d66ecee31b9
Vali Nagacevschi <valentin@minds.com>
2024-02-08 11:09:17 +0000

    add tests

---
+++ src/compose/ComposeCreateScreen.spec.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Keyboard } from 'react-native';
+import { render, fireEvent } from '@testing-library/react-native';
+import ComposeCreateScreen, {
+  pushComposeCreateScreen,
+} from './ComposeCreateScreen';
+import { pushBottomSheet } from '../common/components/bottom-sheet';
+
+jest.mock('../navigation/NavigationService');
+jest.mock('../common/components/bottom-sheet');
+const mockOnItemPress = jest.fn();
+
+describe('ComposeCreateScreen', () => {
+  it('renders correctly', () => {
+    const { getByText } = render(
+      <ComposeCreateScreen onItemPress={mockOnItemPress} />,
+    );
+
+    expect(getByText('Create')).toBeDefined();
+    expect(getByText('Post')).toBeDefined();
+    expect(getByText('Monetized Post')).toBeDefined();
+    expect(getByText('Supermind')).toBeDefined();
+    expect(getByText('Long press')).toBeDefined();
+    expect(getByText('to skip this menu')).toBeDefined();
+  });
+
+  it('calls onItemPress with correct key when menu item is pressed', () => {
+    const { getByText } = render(
+      <ComposeCreateScreen onItemPress={mockOnItemPress} />,
+    );
+
+    fireEvent.press(getByText('Post'));
+    expect(mockOnItemPress).toHaveBeenCalledWith('post');
+
+    fireEvent.press(getByText('Monetized Post'));
+    expect(mockOnItemPress).toHaveBeenCalledWith('monetizedPost');
+
+    fireEvent.press(getByText('Boosted Post'));
+    expect(mockOnItemPress).toHaveBeenCalledWith('boost');
+  });
+});
+
+describe('pushComposeCreateScreen', () => {
+  it('dismisses keyboard and pushes bottom sheet with ComposeCreateScreen', () => {
+    Keyboard.dismiss = jest.fn();
+
+    pushComposeCreateScreen();
+    expect(Keyboard.dismiss).toHaveBeenCalled();
+    expect(pushBottomSheet).toHaveBeenCalledWith(
+      expect.objectContaining({ component: expect.any(Function) }),
+    );
+  });
+});
