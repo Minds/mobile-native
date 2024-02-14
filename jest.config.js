6af4f811ed7143a0ea4e53ecc884a4c8fdc002a8
Martin Santangelo <msantang78@gmail.com>
2024-02-14 10:14:21 -0300

    fix test config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,11 @@
 const config = {
   automock: false,
   cacheDirectory: '.jest/cache',
-  testRegex: ['./__tests__/.*.js$', './src|preview/.*\\.spec\\.[tj]sx?$'],
+  testRegex: [
+    './__tests__/.*.js$',
+    './src/.*\\.spec\\.[tj]sx?$',
+    './preview/.*\\.spec\\.[tj]sx?$',
+  ],
   preset: 'jest-expo',
   setupFiles: ['<rootDir>/setupTests.js'],
   testPathIgnorePatterns: ['/node_modules/', '.yarn'],
