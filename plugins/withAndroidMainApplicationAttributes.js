b344d8fdfcb0769d5b0d3f89796099aa0a5f50a4
Vali Nagacevschi <valentin@minds.com>
2024-02-19 16:36:15 +0000

    add filepath.xml creation

--- plugins/withAndroidMainApplicationAttributes.js
+++ plugins/withAndroidMainApplicationAttributes.js
@@ -3,13 +3,44 @@ const { AndroidConfig, withAndroidManifest } = require('@expo/config-plugins');
 const { getMainApplicationOrThrow, addMetaDataItemToMainApplication } =
   AndroidConfig.Manifest;
 
+const fs = require('fs');
+const path = require('path');
+
+async function saveFileAsync(file_path, content) {
+  return fs.promises.writeFile(file_path, content, 'utf8');
+}
+
+async function createFilepath(config) {
+  const fileDirectory = path.join(
+    config.modRequest.platformProjectRoot,
+    'app',
+    'src',
+    'main',
+    'res',
+    'xml',
+  );
+  const filepathPath = path.join(fileDirectory, 'filepaths.xml');
+
+  try {
+    fs.mkdir(fileDirectory, { recursive: true }, async err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const content =
+        '<?xml version="1.0" encoding="utf-8"?><paths xmlns:android="http://schemas.android.com/apk/res/android"><external-path name="myexternalimages" path="Download/" /><external-path name="external_files" path="."/><cache-path name="cache" path="/" /></paths>';
+      saveFileAsync(filepathPath, content);
+    });
+  } catch (e) {
+    console.error('Error saving file', e);
+  }
+}
+
 function addProviderToMainApplication(androidManifest) {
   const { manifest } = androidManifest;
 
   if (!Array.isArray(manifest.application)) {
-    console.warn(
-      'withWordlLineIntentActivity: No application array in manifest?',
-    );
+    console.warn('withApplicationProvider: No application array in manifest?');
     return androidManifest;
   }
 
@@ -17,12 +48,13 @@ function addProviderToMainApplication(androidManifest) {
     item => item.$['android:name'] === '.MainApplication',
   );
   if (!application) {
-    console.warn('withWordlLineIntentActivity: No .MainApplication?');
+    console.warn('withApplicationProvider: No .MainApplication?');
     return androidManifest;
   }
 
   const meta_data = {};
   meta_data.$ = {
+    ...meta_data.$,
     'android:name': 'android.support.FILE_PROVIDER_PATHS',
     'android:resource': '@xml/filepaths',
     'tools:replace': 'android:resource',
@@ -32,6 +64,7 @@ function addProviderToMainApplication(androidManifest) {
     'meta-data': meta_data,
   };
   provider.$ = {
+    ...provider.$,
     'android:name': 'androidx.core.content.FileProvider',
     'android:authorities': '${applicationId}.provider',
     'android:exported': 'false',
@@ -45,8 +78,9 @@ function addProviderToMainApplication(androidManifest) {
 }
 
 module.exports = function withIntentActivity(config) {
-  return withAndroidManifest(config, config => {
-    config.modResults = addProviderToMainApplication(config.modResults);
-    return config;
+  return withAndroidManifest(config, conf => {
+    conf.modResults = addProviderToMainApplication(conf.modResults);
+    createFilepath(conf);
+    return conf;
   });
 };
