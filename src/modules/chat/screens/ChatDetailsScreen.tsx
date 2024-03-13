6fd4618559af98520a7063d2ede5055efcd7a8e9
Vali Nagacevschi <valentin@minds.com>
2024-03-13 17:31:47 +0000

    add filter

--- src/modules/chat/screens/ChatDetailsScreen.tsx
+++ src/modules/chat/screens/ChatDetailsScreen.tsx
@@ -5,12 +5,30 @@ import Toggle from '~/common/components/Toggle';
 import { B1, H3, IconButton, Row, Screen, ScreenHeader } from '~/common/ui';
 import ThemedStyles from '~/styles/ThemedStyles';
 import Member from '../components/Member';
+import sessionService from '~/common/services/session.service';
 
 export default function ChatDetailsScreen({ route }) {
-  const { members } = route.params ?? {};
-  const [expanded, setExpanded] = useState(false);
+  const myGuid = sessionService.getUser().guid;
+  const members = (route.params?.members ?? []).filter(m => m.guid !== myGuid);
+
+  const [collapsed, setCollapsed] = useState(true);
   const [mute, setMute] = useState(false);
-  const privateChat = (members?.length ?? 0) <= 2;
+
+  const deleteChat = () => {
+    console.log('delete chat');
+  };
+  const leaveChat = () => {
+    console.log('leave chat');
+  };
+
+  const reportUser = () => {
+    console.log('report user', members[0].username);
+  };
+  const blockUser = () => {
+    console.log('block user', members[0].username);
+  };
+
+  const privateChat = (members?.length ?? 0) === 1;
   const myChat = true;
   return (
     <Screen safe scroll>
@@ -25,13 +43,13 @@ export default function ChatDetailsScreen({ route }) {
       <Row align="centerBetween" vertical="XL" horizontal="XXXL">
         <H3>Chat Members ({members?.length})</H3>
         <IconButton
-          name={expanded ? 'chevron-down' : 'chevron-right'}
+          name={collapsed ? 'chevron-right' : 'chevron-down'}
           size={32}
-          onPress={() => setExpanded(prev => !prev)}
+          onPress={() => setCollapsed(prev => !prev)}
         />
       </Row>
       <View style={styles.separator} />
-      <View style={[styles.container, !expanded && { display: 'none' }]}>
+      <View style={[styles.container, collapsed && { display: 'none' }]}>
         {members.map(member => (
           <Member key={member.guid} member={member} />
         ))}
@@ -39,19 +57,25 @@ export default function ChatDetailsScreen({ route }) {
       <View style={styles.separator} />
       {privateChat && (
         <>
-          <Link style={styles.simpleLink} decoration={false} onPress={() => {}}>
+          <Link
+            style={styles.simpleLink}
+            decoration={false}
+            onPress={reportUser}>
             Report user
           </Link>
-          <Link style={styles.dangerLink} decoration={false} onPress={() => {}}>
+          <Link
+            style={styles.dangerLink}
+            decoration={false}
+            onPress={blockUser}>
             Block user
           </Link>
         </>
       )}
-      <Link style={styles.dangerLink} decoration={false} onPress={() => {}}>
+      <Link style={styles.dangerLink} decoration={false} onPress={leaveChat}>
         Leave chat
       </Link>
       {myChat && (
-        <Link style={styles.dangerLink} decoration={false} onPress={() => {}}>
+        <Link style={styles.dangerLink} decoration={false} onPress={deleteChat}>
           Delete chat
         </Link>
       )}
