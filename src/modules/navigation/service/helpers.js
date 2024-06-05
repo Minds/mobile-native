13d5575c0c8fbd3542a49aa53100d19453be0c11
Martin Santangelo <msantang78@gmail.com>
2024-06-05 16:15:11 -0300

    Use the visibleMobile toggle for the custom nav items

--- src/modules/navigation/service/helpers.js
+++ src/modules/navigation/service/helpers.js
@@ -2,7 +2,7 @@ const removeFromMobile = ['memberships', 'admin'];
 
 function filterNavigationItems(items) {
   return items.filter(
-    item => item.visible && !removeFromMobile.includes(item.id),
+    item => item.visibleMobile && !removeFromMobile.includes(item.id),
   );
 }
 
