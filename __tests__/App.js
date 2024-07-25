c6a6371e4a65a883cb06870c4b6516509e291443
Martin Santangelo <msantang78@gmail.com>
2024-07-25 10:21:37 -0300

    fix tests

--- __tests__/App.js
+++ __tests__/App.js
@@ -4,7 +4,6 @@ import { BackHandler } from 'react-native';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import { getStores } from '../AppStores';
-import { Lifetime } from '../src/services/InjectionContainer';
 import { ThemedStyles } from '../src/styles/ThemedStyles';
 
 //mock packages
