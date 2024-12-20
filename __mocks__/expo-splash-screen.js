0d0b504c9b6f6c86d9bad0f3a08bc5c45f208ff1
Martin Santangelo <msantang78@gmail.com>
2024-12-20 17:54:03 -0300

    Fix tests

---
+++ __mocks__/expo-splash-screen.js
@@ -0,0 +1,1 @@
+export const preventAutoHideAsync = jest.fn();
--- src/settings/screens/twitter-sync/TwitterSyncScreen.spec.tsx
+++ src/settings/screens/twitter-sync/TwitterSyncScreen.spec.tsx
@@ -1,7 +1,7 @@
 import 'react-native';
 import React from 'react';
+import { render } from '@testing-library/react-native';
 import TwitterSyncScreen from './TwitterSyncScreen';
-import { shallow } from 'enzyme';
 import { QueryProvider } from '~/services';
 import sp from '~/services/serviceProvider';
 
@@ -10,15 +10,20 @@ jest.mock('react-native-modern-datepicker', () => ({}));
 
 // mock services
 sp.mockService('styles');
+sp.mockService('i18n');
+sp.mockService('session');
+sp.mockService('api');
+const mindsSettings = sp.mockService('config');
 
 describe('TwitterSyncScreen', () => {
+  mindsSettings.getSettings.mockReturnValue({ twitter: null });
   test('renders correctly', () => {
-    const component = shallow(
+    const { toJSON } = render(
       <QueryProvider>
         <TwitterSyncScreen />
       </QueryProvider>,
     );
 
-    expect(component).toMatchSnapshot();
+    expect(toJSON()).toMatchSnapshot();
   });
 });
